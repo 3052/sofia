e175cb884f7966125857fb3f585cc5c71396e3dc
3052 <spen+3052@protonmail.com>
2023-12-23 14:48:26 -0600

    modified:   senc.go

--- moof_test.go
+++ moof_test.go
@@ -1,18 +1,18 @@
 package sofia
 
 import (
-	"os"
-	"testing"
+   "os"
+   "testing"
 )
 
 func Test_Moof(t *testing.T) {
-	media, err := os.Open("testdata/amc-audio/segment0.m4f")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer media.Close()
-	var f File
-	if err := f.Decode(media); err != nil {
-		t.Fatal(err)
-	}
+   media, err := os.Open("testdata/amc-audio/segment0.m4f")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer media.Close()
+   var f File
+   if err := f.Decode(media); err != nil {
+      t.Fatal(err)
+   }
 }

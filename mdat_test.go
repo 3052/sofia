2296262af2eaa35df140820b18f3957e2b49743c
3052 <spen+3052@protonmail.com>
2023-12-23 15:17:17 -0600

    new file:   blog/2023-12-23/test.md

--- mdat_test.go
+++ mdat_test.go
@@ -2,9 +2,40 @@ package sofia
 
 import (
    "encoding/hex"
+   "io"
    "os"
    "testing"
 )
+
+func segment(dst io.Writer) error {
+   src, err := os.Open("testdata/amc-video/segment0.m4f")
+   if err != nil {
+      return err
+   }
+   defer src.Close()
+   var f File
+   if err := f.Decode(src); err != nil {
+      return err
+   }
+   key, err := hex.DecodeString("c58d3308ed18d43776a78232f552dbe0")
+   if err != nil {
+      return err
+   }
+   for i := range f.Mdat.Data {
+      sample := f.Mdat.Data[i]
+      enc := f.Moof.Traf.Senc.Samples[i]
+      err := CryptSampleCenc(sample, key, enc)
+      if err != nil {
+         return err
+      }
+   }
+   for _, b := range f.Moof.Traf.Boxes {
+      if b.Header.Type() == "saiz" {
+         b.Header.RawType = [4]byte{'f', 'r', 'e', 'e'}
+      }
+   }
+   return f.Encode(dst)
+}
 
 func Test_Mdat(t *testing.T) {
    dst, err := os.Create("dec.m4v")
@@ -12,14 +43,7 @@ func Test_Mdat(t *testing.T) {
       t.Fatal(err)
    }
    defer dst.Close()
-   init, err := os.ReadFile("testdata/amc-video/init.m4f")
-   if err != nil {
-      t.Fatal(err)
-   }
-   if _, err := dst.Write(init); err != nil {
-      t.Fatal(err)
-   }
-   src, err := os.Open("testdata/amc-video/segment0.m4f")
+   src, err := os.Open("testdata/amc-video/init.m4f")
    if err != nil {
       t.Fatal(err)
    }
@@ -28,24 +52,10 @@ func Test_Mdat(t *testing.T) {
    if err := f.Decode(src); err != nil {
       t.Fatal(err)
    }
-   key, err := hex.DecodeString("c58d3308ed18d43776a78232f552dbe0")
-   if err != nil {
+   if err := f.Encode(dst); err != nil {
       t.Fatal(err)
    }
-   for i := range f.Mdat.Data {
-      sample := f.Mdat.Data[i]
-      enc := f.Moof.Traf.Senc.Samples[i]
-      err := CryptSampleCenc(sample, key, enc)
-      if err != nil {
-         t.Fatal(err)
-      }
-   }
-   for _, b := range f.Moof.Traf.Boxes {
-      if b.Header.Type() == "saiz" {
-         b.Header.RawType = [4]byte{'f', 'r', 'e', 'e'}
-      }
-   }
-   if err := f.Encode(dst); err != nil {
+   if err := segment(dst); err != nil {
       t.Fatal(err)
    }
 }

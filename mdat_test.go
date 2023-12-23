ae0a9a12b1bc18ebdd68357145cb679c57fe4ed0
3052 <spen+3052@protonmail.com>
2023-12-23 14:36:57 -0600

    +      if b.Header.Type() == "saiz" {

--- mdat_test.go
+++ mdat_test.go
@@ -7,17 +7,29 @@ import (
 )
 
 func Test_Mdat(t *testing.T) {
+   dst, err := os.Create("dec.m4v")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer dst.Close()
+   init, err := os.ReadFile("testdata/amc-video/init.m4f")
+   if err != nil {
+      t.Fatal(err)
+   }
+   if _, err := dst.Write(init); err != nil {
+      t.Fatal(err)
+   }
    src, err := os.Open("testdata/amc-video/segment0.m4f")
    if err != nil {
       t.Fatal(err)
    }
    defer src.Close()
-   key, err := hex.DecodeString("c58d3308ed18d43776a78232f552dbe0")
-   if err != nil {
-      t.Fatal(err)
-   }
    var f File
    if err := f.Decode(src); err != nil {
+      t.Fatal(err)
+   }
+   key, err := hex.DecodeString("c58d3308ed18d43776a78232f552dbe0")
+   if err != nil {
       t.Fatal(err)
    }
    for i := range f.Mdat.Data {
@@ -28,16 +40,11 @@ func Test_Mdat(t *testing.T) {
          t.Fatal(err)
       }
    }
-   init, err := os.ReadFile("testdata/amc-video/init.m4f")
-   if err != nil {
-      t.Fatal(err)
+   for _, b := range f.Moof.Traf.Boxes {
+      if b.Header.Type() == "saiz" {
+         b.Header.RawType = [4]byte{'f', 'r', 'e', 'e'}
+      }
    }
-   dst, err := os.Create("dec.m4v")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer dst.Close()
-   dst.Write(init)
    if err := f.Encode(dst); err != nil {
       t.Fatal(err)
    }
--- traf.go
+++ traf.go
@@ -1,69 +1,69 @@
 package sofia
 
 import (
-	"fmt"
-	"io"
+   "fmt"
+   "io"
 )
 
 // aligned(8) class TrackFragmentBox extends Box('traf') {
 // }
 type TrackFragmentBox struct {
-	Header BoxHeader
-	Boxes  []Box
-	Trun   TrackRunBox
-	Senc   SampleEncryptionBox
-}
-
-func (t TrackFragmentBox) Encode(dst io.Writer) error {
-	err := t.Header.Encode(dst)
-	if err != nil {
-		return err
-	}
-	for _, b := range t.Boxes {
-		err := b.Encode(dst)
-		if err != nil {
-			return err
-		}
-	}
-	if err := t.Trun.Encode(dst); err != nil {
-		return err
-	}
-	return t.Senc.Encode(dst)
+   Header BoxHeader
+   Boxes  []*Box
+   Trun   TrackRunBox
+   Senc   SampleEncryptionBox
 }
 
 func (t *TrackFragmentBox) Decode(src io.Reader) error {
-	for {
-		var head BoxHeader
-		err := head.Decode(src)
-		if err == io.EOF {
-			return nil
-		} else if err != nil {
-			return err
-		}
-		size := head.BoxPayload()
-		switch head.Type() {
-		case "senc":
-			t.Senc.BoxHeader = head
-			err := t.Senc.Decode(src)
-			if err != nil {
-				return err
-			}
-		case "trun":
-			t.Trun.BoxHeader = head
-			err := t.Trun.Decode(src)
-			if err != nil {
-				return err
-			}
-		case "saio", "saiz", "sbgp", "sgpd", "tfdt", "tfhd", "uuid":
-			b := Box{Header: head}
-			b.Payload = make([]byte, size)
-			_, err := src.Read(b.Payload)
-			if err != nil {
-				return err
-			}
-			t.Boxes = append(t.Boxes, b)
-		default:
-			return fmt.Errorf("%q", head.RawType)
-		}
-	}
+   for {
+      var head BoxHeader
+      err := head.Decode(src)
+      if err == io.EOF {
+         return nil
+      } else if err != nil {
+         return err
+      }
+      size := head.BoxPayload()
+      switch head.Type() {
+      case "senc":
+         t.Senc.BoxHeader = head
+         err := t.Senc.Decode(src)
+         if err != nil {
+            return err
+         }
+      case "trun":
+         t.Trun.BoxHeader = head
+         err := t.Trun.Decode(src)
+         if err != nil {
+            return err
+         }
+      case "saio", "saiz", "sbgp", "sgpd", "tfdt", "tfhd", "uuid":
+         b := Box{Header: head}
+         b.Payload = make([]byte, size)
+         _, err := src.Read(b.Payload)
+         if err != nil {
+            return err
+         }
+         t.Boxes = append(t.Boxes, &b)
+      default:
+         return fmt.Errorf("%q", head.RawType)
+      }
+   }
+}
+
+func (t TrackFragmentBox) Encode(dst io.Writer) error {
+   err := t.Header.Encode(dst)
+   if err != nil {
+      return err
+   }
+   for _, b := range t.Boxes {
+      err := b.Encode(dst)
+      if err != nil {
+         return err
+      }
+   }
+   if err := t.Trun.Encode(dst); err != nil {
+      return err
+   }
+   return t.Senc.Encode(dst)
 }

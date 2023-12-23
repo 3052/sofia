49724657c8b2dde8bd6b0cddbe454ce60ec10ec0
3052 <spen+3052@protonmail.com>
2023-12-23 11:09:56 -0600

    +func CryptSampleCenc(

--- mdat_test.go
+++ mdat_test.go
@@ -1,23 +1,23 @@
 package sofia
 
 import (
-   "fmt"
-   "os"
-   "testing"
+	"fmt"
+	"os"
+	"testing"
 )
 
 func Test_Mdat(t *testing.T) {
-   media, err := os.Open("testdata/amc-video/segment0.m4f")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer media.Close()
-   var f File
-   if err := f.Decode(media); err != nil {
-      t.Fatal(err)
-   }
-   for _, data := range f.Mdat.Data {
-      fmt.Println(len(data))
-   }
-   fmt.Println("len(f.Mdat.Data)", len(f.Mdat.Data))
+	media, err := os.Open("testdata/amc-video/segment0.m4f")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer media.Close()
+	var f File
+	if err := f.Decode(media); err != nil {
+		t.Fatal(err)
+	}
+	for _, data := range f.Mdat.Data {
+		fmt.Println(len(data))
+	}
+	fmt.Println("len(f.Mdat.Data)", len(f.Mdat.Data))
 }

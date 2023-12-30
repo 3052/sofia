de60f2ab39068ae35fedfc030c867bfbd4178f1f
3052 <spen+3052@protonmail.com>
2023-12-30 17:31:43 -0600

    new file:   OriginalFormatBox_test.go

---
+++ OriginalFormatBox_test.go
@@ -0,0 +1,22 @@
+package sofia
+
+import (
+   "fmt"
+   "os"
+   "testing"
+)
+
+func Test_OriginalFormat(t *testing.T) {
+   media, err := os.Open("testdata/hulu-video/init.mp4")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer media.Close()
+   var f File
+   if err := f.Decode(media); err != nil {
+      t.Fatal(err)
+   }
+   for _, byte_range := range f.SegmentIndex.ByteRanges(0) {
+      fmt.Println(byte_range)
+   }
+}

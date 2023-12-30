d0880d37b0dce34ca2eb45a4745eb2b406088eeb
3052 <spen+3052@protonmail.com>
2023-12-29 20:51:42 -0600

    new file:   testdata/paramount-video/seg_1303.m4s

--- tfhd.go
+++ tfhd.go
@@ -1,62 +1,96 @@
 package sofia
 
 import (
-   "errors"
+   "encoding/binary"
    "io"
-   "log/slog"
 )
 
-// 8.3.1 Track box
-//  aligned(8) class TrackBox extends Box('trak') {
+// 8.8.7 Track fragment header box
+//  aligned(8) class TrackFragmentHeaderBox extends FullBox(
+//     'tfhd', 0, tf_flags
+//  ) {
+//     unsigned int(32) track_ID;
+//     // all the following are optional fields
+//     // their presence is indicated by bits in the tf_flags
+//     unsigned int(64) base_data_offset;
+//     unsigned int(32) sample_description_index;
+//     unsigned int(32) default_sample_duration;
+//     unsigned int(32) default_sample_size;
+//     unsigned int(32) default_sample_flags;
 //  }
-type TrackBox struct {
-   Header  BoxHeader
-   Boxes []Box
-   Mdia MediaBox
+type TrackFragmentHeaderBox struct {
+   BoxHeader          BoxHeader
+   FullBoxHeader      FullBoxHeader
+   Track_ID uint32
+   Base_Data_Offset uint64
+   Sample_Description_Index uint32
+   Default_Sample_Duration uint32
+   Default_Sample_Size uint32
+   Default_Sample_Flags uint32
 }
 
-func (b *TrackBox) Decode(r io.Reader) error {
-   for {
-      var head BoxHeader
-      err := head.Decode(r)
-      if err == io.EOF {
-         return nil
-      } else if err != nil {
-         return err
-      }
-      slog.Debug("*", "BoxType", head.BoxType())
-      size := head.BoxPayload()
-      switch head.BoxType() {
-      case "edts", "tkhd":
-         value := Box{Header: head}
-         value.Payload = make([]byte, size)
-         _, err := io.ReadFull(r, value.Payload)
-         if err != nil {
-            return err
-         }
-         b.Boxes = append(b.Boxes, value)
-      case "mdia":
-         b.Mdia.Header = head
-         err := b.Mdia.Decode(io.LimitReader(r, size))
-         if err != nil {
-            return err
-         }
-      default:
-         return errors.New("BoxType")
-      }
-   }
-}
-
-func (b TrackBox) Encode(w io.Writer) error {
-   err := b.Header.Encode(w)
+func (b *TrackFragmentHeaderBox) Decode(r io.Reader) error {
+   err := b.FullBoxHeader.Decode(r)
    if err != nil {
       return err
    }
-   for _, value := range b.Boxes {
-      err := value.Encode(w)
+   if err := binary.Read(r, binary.BigEndian, &b.Track_ID); err != nil {
+      return err
+   }
+   if b.Base_Data_Offset_Present() {
+      err := binary.Read(r, binary.BigEndian, &b.Base_Data_Offset)
       if err != nil {
          return err
       }
    }
-   return b.Mdia.Encode(w)
+   if b.Sample_Description_Index_Present() {
+      err := binary.Read(r, binary.BigEndian, &b.Sample_Description_Index)
+      if err != nil {
+         return err
+      }
+   }
+   if b.Default_Sample_Duration_Present() {
+      err := binary.Read(r, binary.BigEndian, &b.Default_Sample_Duration)
+      if err != nil {
+         return err
+      }
+   }
+   if b.Default_Sample_Size_Present() {
+      err := binary.Read(r, binary.BigEndian, &b.Default_Sample_Size)
+      if err != nil {
+         return err
+      }
+   }
+   if b.Default_Sample_Flags_Present() {
+      err := binary.Read(r, binary.BigEndian, &b.Default_Sample_Flags)
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+// 0x000001 base-data-offset-present
+func (b TrackFragmentHeaderBox) Base_Data_Offset_Present() bool {
+   return b.FullBoxHeader.Flags() & 1 >= 1
+}
+
+// 0x000002 sample-description-index-present
+func (b TrackFragmentHeaderBox) Sample_Description_Index_Present() bool {
+   return b.FullBoxHeader.Flags() & 2 >= 1
+}
+
+// 0x000008 default-sample-duration-present
+func (b TrackFragmentHeaderBox) Default_Sample_Duration_Present() bool {
+   return b.FullBoxHeader.Flags() & 8 >= 1
+}
+
+// 0x000010 default-sample-size-present
+func (b TrackFragmentHeaderBox) Default_Sample_Size_Present() bool {
+   return b.FullBoxHeader.Flags() & 0x10 >= 1
+}
+
+// 0x000020 default-sample-flags-present
+func (b TrackFragmentHeaderBox) Default_Sample_Flags_Present() bool {
+   return b.FullBoxHeader.Flags() & 0x20 >= 1
 }

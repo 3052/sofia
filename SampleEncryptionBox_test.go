755bb702436cab6f78ebe46a18a808e44175a72a
3052 <spen+3052@protonmail.com>
2023-12-30 18:20:28 -0600

    +   DataFormat [4]uint8

--- SampleEncryptionBox_test.go
+++ SampleEncryptionBox_test.go
@@ -7,6 +7,69 @@ import (
    "testing"
 )
 
+var tests = []testdata{
+   {
+      "testdata/amc-mp4a/init.m4f",
+      "testdata/amc-mp4a/segment0.m4f",
+      "91d888dfb0562ebc3abdd845d451e858",
+      "amc-mp4a.mp4",
+   },
+   {
+      "testdata/amc-avc1/init.m4f",
+      "testdata/amc-avc1/segment0.m4f",
+      "c58d3308ed18d43776a78232f552dbe0",
+      "amc-avc1.mp4",
+   },
+   {
+      "testdata/hulu-mp4a/init.mp4",
+      "testdata/hulu-mp4a/segment-1.0001.m4s",
+      "602a9289bfb9b1995b75ac63f123fc86",
+      "hulu-mp4a.mp4",
+   },
+   {
+      "testdata/hulu-avc1/init.mp4",
+      "testdata/hulu-avc1/segment-1.0001.m4s",
+      "602a9289bfb9b1995b75ac63f123fc86",
+      "hulu-avc1.mp4",
+   },
+   {
+      "testdata/nbc-mp4a/_227156876_6_1.mp4",
+      "testdata/nbc-mp4a/_227156876_6_1_0.mp4",
+      "3e2e8ccff89d0a72598a347feab5e7c8",
+      "nbc-mp4a.mp4",
+   },
+   {
+      "testdata/nbc-avc1/_227156876_5.mp4",
+      "testdata/nbc-avc1/_227156876_5_0.mp4",
+      "3e2e8ccff89d0a72598a347feab5e7c8",
+      "nbc-avc1.mp4",
+   },
+   {
+      "testdata/paramount-mp4a/init.m4v",
+      "testdata/paramount-mp4a/seg_1.m4s",
+      "d98277ff6d7406ec398b49bbd52937d4",
+      "paramount-mp4a.mp4",
+   },
+   {
+      "testdata/paramount-avc1/init.m4v",
+      "testdata/paramount-avc1/seg_1.m4s",
+      "d98277ff6d7406ec398b49bbd52937d4",
+      "paramount-avc1.mp4",
+   },
+   {
+      "testdata/roku-mp4a/index_audio_2_0_init.mp4",
+      "testdata/roku-mp4a/index_audio_2_0_1.mp4",
+      "1ba08384626f9523e37b9db17f44da2b",
+      "roku-mp4a.mp4",
+   },
+   {
+      "testdata/roku-avc1/index_video_8_0_init.mp4",
+      "testdata/roku-avc1/index_video_8_0_1.mp4",
+      "1ba08384626f9523e37b9db17f44da2b",
+      "roku-avc1.mp4",
+   },
+}
+
 func Test_SampleEncryption(t *testing.T) {
    for _, test := range tests {
       func() {
@@ -23,6 +86,37 @@ func Test_SampleEncryption(t *testing.T) {
          }
       }()
    }
+}
+
+func (t testdata) encode_segment(dst io.Writer) error {
+   src, err := os.Open(t.segment)
+   if err != nil {
+      return err
+   }
+   defer src.Close()
+   var f File
+   if err := f.Decode(src); err != nil {
+      return err
+   }
+   key, err := hex.DecodeString(t.key)
+   if err != nil {
+      return err
+   }
+   for i, data := range f.MediaData.Data {
+      sample := f.MovieFragment.TrackFragment.SampleEncryption.Samples[i]
+      err := sample.Decrypt_CENC(data, key)
+      if err != nil {
+         return err
+      }
+   }
+   return f.Encode(dst)
+}
+
+type testdata struct {
+   init string
+   segment string
+   key string
+   out string
 }
 
 func (t testdata) encode_init(dst io.Writer) error {
@@ -52,100 +146,9 @@ func (t testdata) encode_init(dst io.Writer) error {
          copy(b.Header.Type[:], "free") // Firefox
       }
    }
-   copy(desc.AudioSample.Entry.Header.Type[:], "mp4a") // Firefox
+   copy(
+      desc.AudioSample.Entry.Header.Type[:],
+      desc.AudioSample.ProtectionScheme.OriginalFormat.DataFormat[:],
+   ) // Firefox
    return f.Encode(dst)
-}
-
-func (t testdata) encode_segment(dst io.Writer) error {
-   src, err := os.Open(t.segment)
-   if err != nil {
-      return err
-   }
-   defer src.Close()
-   var f File
-   if err := f.Decode(src); err != nil {
-      return err
-   }
-   key, err := hex.DecodeString(t.key)
-   if err != nil {
-      return err
-   }
-   for i, data := range f.MediaData.Data {
-      sample := f.MovieFragment.TrackFragment.SampleEncryption.Samples[i]
-      err := sample.Decrypt_CENC(data, key)
-      if err != nil {
-         return err
-      }
-   }
-   return f.Encode(dst)
-}
-
-var tests = []testdata{
-   {
-      "testdata/amc-audio/init.m4f",
-      "testdata/amc-audio/segment0.m4f",
-      "91d888dfb0562ebc3abdd845d451e858",
-      "amc-audio.mp4",
-   },
-   {
-      "testdata/amc-video/init.m4f",
-      "testdata/amc-video/segment0.m4f",
-      "c58d3308ed18d43776a78232f552dbe0",
-      "amc-video.mp4",
-   },
-   {
-      "testdata/hulu-audio/init.mp4",
-      "testdata/hulu-audio/segment-1.0001.m4s",
-      "602a9289bfb9b1995b75ac63f123fc86",
-      "hulu-audio.mp4",
-   },
-   {
-      "testdata/hulu-video/init.mp4",
-      "testdata/hulu-video/segment-1.0001.m4s",
-      "602a9289bfb9b1995b75ac63f123fc86",
-      "hulu-video.mp4",
-   },
-   {
-      "testdata/nbc-audio/_227156876_6_1.mp4",
-      "testdata/nbc-audio/_227156876_6_1_0.mp4",
-      "3e2e8ccff89d0a72598a347feab5e7c8",
-      "nbc-audio.mp4",
-   },
-   {
-      "testdata/nbc-video/_227156876_5.mp4",
-      "testdata/nbc-video/_227156876_5_0.mp4",
-      "3e2e8ccff89d0a72598a347feab5e7c8",
-      "nbc-video.mp4",
-   },
-   {
-      "testdata/paramount-audio/init.m4v",
-      "testdata/paramount-audio/seg_1.m4s",
-      "d98277ff6d7406ec398b49bbd52937d4",
-      "paramount-audio.mp4",
-   },
-   {
-      "testdata/paramount-video/init.m4v",
-      "testdata/paramount-video/seg_1.m4s",
-      "d98277ff6d7406ec398b49bbd52937d4",
-      "paramount-video.mp4",
-   },
-   {
-      "testdata/roku-audio/index_audio_2_0_init.mp4",
-      "testdata/roku-audio/index_audio_2_0_1.mp4",
-      "1ba08384626f9523e37b9db17f44da2b",
-      "roku-audio.mp4",
-   },
-   {
-      "testdata/roku-video/index_video_8_0_init.mp4",
-      "testdata/roku-video/index_video_8_0_1.mp4",
-      "1ba08384626f9523e37b9db17f44da2b",
-      "roku-video.mp4",
-   },
-}
-
-type testdata struct {
-   init string
-   segment string
-   key string
-   out string
 }

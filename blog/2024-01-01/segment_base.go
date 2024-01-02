46f662a6f9f5fdf98e478e46adb0715de471be69
3052 <spen+3052@protonmail.com>
2024-01-01 19:09:48 -0600

    +*.m4s

--- blog/2024-01-01/segment_base.go
+++ blog/2024-01-01/segment_base.go
@@ -6,6 +6,33 @@ import (
    "os"
 )
 
+func file_copy() error {
+   file, err := os.Create("dec.mp4")
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   var (
+      sidx uint32 = 1530
+      moof uint32 = 16178
+   )
+   data, err := os.ReadFile("enc.mp4")
+   if err != nil {
+      return err
+   }
+   file.Write(data[:sidx])
+   file.Write(data[sidx:moof])
+   byte_ranges, err := decode_sidx(data, sidx, moof)
+   if err != nil {
+      return err
+   }
+   for _, r := range byte_ranges {
+      segment := data[r[0]:r[1]+1]
+      file.Write(segment)
+   }
+   return nil
+}
+
 func segment_base() error {
    key, err := hex.DecodeString("dee726e9015a608a3db559a6b9a9c034")
    if err != nil {

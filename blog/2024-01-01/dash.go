979903b028e72e48546b50cb25adcf2d5be4de28
3052 <spen+3052@protonmail.com>
2024-01-01 19:59:55 -0600

    deleted:    blog/2024-01-01/segment_base.go

--- blog/2024-01-01/dash.go
+++ blog/2024-01-01/dash.go
@@ -3,7 +3,10 @@ package stream
 import (
    "154.pages.dev/sofia"
    "bytes"
+   "encoding/hex"
+   "fmt"
    "io"
+   "os"
 )
 
 func decode_sidx(data []byte, sidx, moof uint32) ([][2]uint32, error) {
@@ -11,7 +14,46 @@ func decode_sidx(data []byte, sidx, moof uint32) ([][2]uint32, error) {
    if err := f.Decode(bytes.NewReader(data[sidx:moof])); err != nil {
       return nil, err
    }
+   for _, ref := range f.SegmentIndex.References {
+      fmt.Println(ref[0], ref.Referenced_Size())
+   }
    return f.SegmentIndex.ByteRanges(moof), nil
+}
+
+func segment_base() error {
+   key, err := hex.DecodeString("dee726e9015a608a3db559a6b9a9c034")
+   if err != nil {
+      return err
+   }
+   file, err := os.Create("dec.mp4")
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   var (
+      sidx uint32 = 1530
+      moof uint32 = 16178
+   )
+   data, err := os.ReadFile("enc.mp4")
+   if err != nil {
+      return err
+   }
+   if err := encode_init(file, bytes.NewReader(data[:sidx])); err != nil {
+      return err
+   }
+   file.Write(data[sidx:moof])
+   byte_ranges, err := decode_sidx(data, sidx, moof)
+   if err != nil {
+      return err
+   }
+   for _, r := range byte_ranges {
+      segment := data[r[0]:r[1]+1]
+      err := encode_segment(file, bytes.NewReader(segment), key)
+      if err != nil {
+         return err
+      }
+   }
+   return nil
 }
 
 func encode_segment(dst io.Writer, src io.Reader, key []byte) error {

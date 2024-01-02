aace323861d95a094412d8c07031fd4b5f9a7d0b
3052 <spen+3052@protonmail.com>
2024-01-01 19:59:55 -0600

    deleted:    blog/2024-01-01/segment_base.go

--- blog/2024-01-01/dash_test.go
+++ blog/2024-01-01/dash_test.go
@@ -1,62 +1,18 @@
 package stream
 
-import (
-   "154.pages.dev/sofia"
-   "os"
-   "testing"
-)
+import "testing"
 
-func Test_Copy(t *testing.T) {
-   err := file_copy()
-   if err != nil {
-      t.Fatal(err)
-   }
-}
-
+/*
+[mfra] size=23190
+  [tfra] size=23166 version=1 flags=000000
+   - trackID: 1
+   - nrEntries: 1218
+  [mfro] size=16 version=0 flags=000000
+   - parentSize: 23190
+*/
 func Test_Decrypt(t *testing.T) {
    err := segment_base()
    if err != nil {
       t.Fatal(err)
    }
 }
-
-func Test_Segment(t *testing.T) {
-   src, err := os.Open("segment-1.0001.m4s")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer src.Close()
-   dst, err := os.Create("out.mp4")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer dst.Close()
-   var file sofia.File
-   if err := file.Decode(src); err != nil {
-      t.Fatal(err)
-   }
-   if err := file.Encode(dst); err != nil {
-      t.Fatal(err)
-   }
-}
-
-func Test_Init(t *testing.T) {
-   src, err := os.Open("init.mp4")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer src.Close()
-   dst, err := os.Create("out.mp4")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer dst.Close()
-   var file sofia.File
-   if err := file.Decode(src); err != nil {
-      t.Fatal(err)
-   }
-   if err := file.Encode(dst); err != nil {
-      t.Fatal(err)
-   }
-}
-

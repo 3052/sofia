1e7e8a020b31bc25fa17690d3ba8feb2be2eca49
3052 <spen+3052@protonmail.com>
2024-01-06 15:13:11 -0600

    new file:   blog/2024-01-06/movflags.go

---
+++ blog/2024-01-06/movflags.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+   "fmt"
+   "os/exec"
+)
+
+var movflags = []string{
+   "",
+   "rtphint",
+   "empty_moov",
+   "frag_keyframe",
+   "frag_every_frame",
+   "separate_moof",
+   "frag_custom",
+   "isml",
+   "faststart",
+   "omit_tfhd_offset",
+   "disable_chpl",
+   "default_base_moof",
+   "dash",
+   "cmaf",
+   "frag_discont",
+   "delay_moov",
+   "global_sidx",
+   "skip_sidx",
+   "write_colr",
+   "prefer_icc",
+   "write_gama",
+   "use_metadata_tags",
+   "skip_trailer",
+   "negative_cts_offsets",
+}
+
+func main() {
+   for _, flag := range movflags {
+      arg := []string{
+         "-i", "BCRhBaFqtf0.mp4",
+         "-c", "copy",
+         "-frag_size", "6M",
+      }
+      if flag != "" {
+         arg = append(arg, "-movflags", flag)
+      }
+      arg = append(arg, flag + ".mp4")
+      cmd := exec.Command("ffmpeg", arg...)
+      fmt.Println(cmd.Args)
+      err := cmd.Run()
+      if err != nil {
+         panic(err)
+      }
+   }
+}

239deb8aba71afea65780c0f49f055fddbabcb60
3052 <spen+3052@protonmail.com>
2024-01-09 18:33:55 -0600

    new file:   blog/2024-01-09/readme.md

--- blog/2024-01-09/ffmpeg.go
+++ blog/2024-01-09/ffmpeg.go
@@ -2,64 +2,72 @@ package main
 
 import (
    "fmt"
-   "os"
    "os/exec"
+   "strings"
 )
 
-var movflags = []string{
-   "",
-   "rtphint",
-   "empty_moov",
-   "frag_keyframe",
-   "frag_every_frame",
-   "separate_moof",
-   "frag_custom",
-   "cmaf",
-   "dash",
-   "default_base_moof",
-   "delay_moov",
-   "disable_chpl",
-   "faststart",
-   "frag_discont",
-   "global_sidx",
-   "negative_cts_offsets",
-   "omit_tfhd_offset",
-   "prefer_icc",
-   "skip_sidx",
-   "skip_trailer",
-   "use_metadata_tags",
-   "write_colr",
-   "write_gama",
+var flags = [][]string{
+   nil,
+   { "-empty_hdlr_name", "1"},
+   { "-frag_interleave", "999"},
+   { "-fragment_index", "2"},
+   { "-iods_audio_profile", "0"},
+   { "-iods_audio_profile", "1"},
+   { "-iods_video_profile", "0"},
+   { "-iods_video_profile", "1"},
+   { "-ism_lookahead", "1"},
+   { "-mov_gamma", "1"},
+   { "-movflags", "cmaf"},
+   { "-movflags", "dash"},
+   { "-movflags", "default_base_moof"},
+   { "-movflags", "delay_moov"},
+   { "-movflags", "disable_chpl"},
+   { "-movflags", "empty_moov"},
+   { "-movflags", "faststart"},
+   { "-movflags", "frag_custom"},
+   { "-movflags", "frag_discont"},
+   { "-movflags", "frag_every_frame"},
+   { "-movflags", "frag_keyframe"},
+   { "-movflags", "global_sidx"},
+   { "-movflags", "isml"},
+   { "-movflags", "negative_cts_offsets"},
+   { "-movflags", "omit_tfhd_offset"},
+   { "-movflags", "prefer_icc"},
+   { "-movflags", "rtphint" },
+   { "-movflags", "separate_moof"},
+   { "-movflags", "skip_sidx"},
+   { "-movflags", "skip_trailer"},
+   { "-movflags", "use_metadata_tags"},
+   { "-movflags", "write_colr"},
+   { "-movflags", "write_gama"},
+   { "-movie_timescale", "1"},
+   { "-skip_iods", "0"},
+   { "-use_editlist", "0"},
+   { "-use_editlist", "1"},
+   { "-use_stream_ids_as_track_ids", "1"},
+   { "-video_track_timescale", "1"},
+   { "-write_btrt", "0"},
+   { "-write_btrt", "1"},
+   { "-write_prft", "pts"},
+   { "-write_prft", "wallclock"},
+   { "-write_tmcd", "0"},
+   { "-write_tmcd", "1"},
 }
 
 func main() {
-   for _, flag := range movflags {
+   for _, flag := range flags {
       arg := []string{
-         "-i", "in.mp4",
+         "-i", "frag.mp4",
          "-c", "copy",
+         "-frag_size", "9K",
       }
-      if flag != "" {
-         arg = append(arg, "-movflags", flag)
-      }
-      arg = append(arg, flag + ".mp4")
+      arg = append(arg, flag...)
+      arg = append(arg, "--", strings.Join(flag, ",") + ".mp4")
       cmd := exec.Command("ffmpeg", arg...)
       fmt.Println(cmd.Args)
       err := cmd.Run()
       if err != nil {
          panic(err)
       }
-      func() {
-         file, err := os.Create(flag + ".txt")
-         if err != nil {
-            panic(err)
-         }
-         defer file.Close()
-         cmd := exec.Command("mp4ff-info", flag + ".mp4")
-         cmd.Stdout = file
-         fmt.Println(cmd.Args)
-         if err := cmd.Run(); err != nil {
-            panic(err)
-         }
-      }()
    }
 }

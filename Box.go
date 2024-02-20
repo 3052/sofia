1d9032dea4ef7c669e9944f16640d30781ce44fc
3052 <spen+3052@protonmail.com>
2024-02-20 14:10:22 -0600

    +//  aligned(8) class BoxHeader (

--- Box.go
+++ Box.go
@@ -7,46 +7,35 @@ import (
 )
 
 // 4.2.2 Object definitions
-//  aligned(8) class Box (
+//  aligned(8) class BoxHeader (
 //     unsigned int(32) boxtype,
 //     optional unsigned int(8)[16] extended_type
 //  ) {
-//     BoxHeader(boxtype, extended_type);
-//     // the remaining bytes are the BoxPayload
+//     unsigned int(32) size;
+//     unsigned int(32) type = boxtype;
+//     if (size==1) {
+//        unsigned int(64) largesize;
+//     } else if (size==0) {
+//        // box extends to end of file
+//     }
+//     if (boxtype=='uuid') {
+//        unsigned int(8)[16] usertype = extended_type;
+//     }
 //  }
-type Box struct {
-   BoxHeader BoxHeader
-   Payload []byte
+type BoxHeader struct {
+   BoxSize uint32
+   Type [4]uint8
+   UserType [16]uint8
 }
 
-func (b *Box) Decode(r io.Reader) error {
-   var err error
-   b.Payload, err = io.ReadAll(r)
-   if err != nil {
-      return err
-   }
-   return nil
-}
-
-func (b Box) Encode(w io.Writer) error {
-   err := b.BoxHeader.Encode(w)
-   if err != nil {
-      return err
-   }
-   if _, err := w.Write(b.Payload); err != nil {
-      return err
-   }
-   return nil
+func (b BoxHeader) BoxPayload(r io.Reader) io.Reader {
+   n := int64(b.BoxSize - b.Size())
+   return io.LimitReader(r, n)
 }
 
 // unsigned int(32) type = boxtype;
 func (b BoxHeader) BoxType() string {
    return string(b.Type[:])
-}
-
-// unsigned int(8)[16] usertype = extended_type;
-func (b BoxHeader) Extended_Type() string {
-   return hex.EncodeToString(b.UserType[:])
 }
 
 func (b *BoxHeader) Decode(r io.Reader) error {
@@ -82,31 +71,51 @@ func (b BoxHeader) Encode(w io.Writer) error {
    return nil
 }
 
+// unsigned int(8)[16] usertype = extended_type;
+func (b BoxHeader) Extended_Type() string {
+   return hex.EncodeToString(b.UserType[:])
+}
+
+func (b BoxHeader) Size() uint32 {
+   var s uint32 = 4 // BoxSize
+   s += 4 // Type
+   if b.BoxType() == "uuid" {
+      s += 16 // UserType
+   }
+   return s
+}
+
 // 4.2.2 Object definitions
-//  aligned(8) class BoxHeader (
+//  aligned(8) class Box (
 //     unsigned int(32) boxtype,
 //     optional unsigned int(8)[16] extended_type
 //  ) {
-//     unsigned int(32) size;
-//     unsigned int(32) type = boxtype;
-//     if (size==1) {
-//        unsigned int(64) largesize;
-//     } else if (size==0) {
-//        // box extends to end of file
-//     }
-//     if (boxtype=='uuid') {
-//        unsigned int(8)[16] usertype = extended_type;
-//     }
+//     BoxHeader(boxtype, extended_type);
+//     // the remaining bytes are the BoxPayload
 //  }
-type BoxHeader struct {
-   BoxSize uint32
-   Type [4]uint8
-   UserType [16]uint8
+type Box struct {
+   BoxHeader BoxHeader
+   Payload []byte
 }
 
-func (b BoxHeader) BoxPayload(r io.Reader) io.Reader {
-   n := int64(b.BoxSize - b.Size())
-   return io.LimitReader(r, n)
+func (b *Box) Decode(r io.Reader) error {
+   var err error
+   b.Payload, err = io.ReadAll(r)
+   if err != nil {
+      return err
+   }
+   return nil
+}
+
+func (b Box) Encode(w io.Writer) error {
+   err := b.BoxHeader.Encode(w)
+   if err != nil {
+      return err
+   }
+   if _, err := w.Write(b.Payload); err != nil {
+      return err
+   }
+   return nil
 }
 
 func (f *FullBoxHeader) Decode(r io.Reader) error {
@@ -121,15 +130,6 @@ func (f FullBoxHeader) Flags() uint32 {
    var b [4]byte
    copy(b[1:], f.RawFlags[:])
    return binary.BigEndian.Uint32(b[:])
-}
-
-func (b BoxHeader) Size() uint32 {
-   var s uint32 = 4 // BoxSize
-   s += 4 // Type
-   if b.BoxType() == "uuid" {
-      s += 16 // UserType
-   }
-   return s
 }
 
 // 4.2.2 Object definitions

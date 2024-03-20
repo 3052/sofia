e0be69fbff7aaceed990e2a545444cf0c6066376
3052 <spen+3052@protonmail.com>
2024-03-19 21:53:33 -0500

    modified:   File.go

--- MediaInformation.go
+++ MediaInformation.go
@@ -16,40 +16,6 @@ type MediaInformation struct {
    SampleTable SampleTable
 }
 
-func (m *MediaInformation) read(r io.Reader) error {
-   for {
-      var head BoxHeader
-      err := head.read(r)
-      if err == io.EOF {
-         return nil
-      } else if err != nil {
-         return err
-      }
-      box_type := head.GetType()
-      r := head.payload(r)
-      slog.Debug("BoxHeader", "Type", box_type)
-      switch box_type {
-      case "dinf", // Roku
-         "smhd", // Roku
-         "vmhd": // Roku
-         b := Box{BoxHeader: head}
-         err := b.read(r)
-         if err != nil {
-            return err
-         }
-         m.Boxes = append(m.Boxes, b)
-      case "stbl":
-         m.SampleTable.BoxHeader = head
-         err := m.SampleTable.read(r)
-         if err != nil {
-            return err
-         }
-      default:
-         return errors.New("MediaInformation.Decode")
-      }
-   }
-}
-
 func (m MediaInformation) write(w io.Writer) error {
    err := m.BoxHeader.write(w)
    if err != nil {
@@ -62,4 +28,40 @@ func (m MediaInformation) write(w io.Writer) error {
       }
    }
    return m.SampleTable.write(w)
+}
+
+func (m *MediaInformation) read(r io.Reader) error {
+   for {
+      var head BoxHeader
+      err := head.read(r)
+      if err == io.EOF {
+         return nil
+      } else if err != nil {
+         return err
+      }
+      box_type := head.GetType()
+      slog.Debug("BoxHeader", "Type", box_type)
+      ///////////////////////////////////////////////////////////////////////////
+      r := head.payload(r)
+      switch box_type {
+      case "stbl":
+         m.SampleTable.BoxHeader = head
+         err := m.SampleTable.read(r)
+         if err != nil {
+            return err
+         }
+      ///////////////////////////////////////////////////////////////////////////
+      case "dinf", // Roku
+         "smhd", // Roku
+         "vmhd": // Roku
+         b := Box{BoxHeader: head}
+         err := b.read(r)
+         if err != nil {
+            return err
+         }
+         m.Boxes = append(m.Boxes, b)
+      default:
+         return errors.New("MediaInformation.Decode")
+      }
+   }
 }

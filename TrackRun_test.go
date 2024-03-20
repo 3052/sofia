2a1f11ab5c94ffc3d7c2c3dd48f9f9eb12b8230f
3052 <spen+3052@protonmail.com>
2024-03-19 20:50:11 -0500

    +func (f *File) Read(r io.Reader) error {

--- TrackRun_test.go
+++ TrackRun_test.go
@@ -1,23 +1,23 @@
 package sofia
 
 import (
-   "log/slog"
-   "os"
-   "testing"
+	"log/slog"
+	"os"
+	"testing"
 )
 
 func TestTrackRun(t *testing.T) {
-   h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-      Level: slog.LevelDebug,
-   })
-   slog.SetDefault(slog.New(h))
-   seg, err := os.Open("testdata/paramount-avc1/seg_1.m4s")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer seg.Close()
-   var f File
-   if err := f.Decode(seg); err != nil {
-      t.Fatal(err)
-   }
+	h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+	slog.SetDefault(slog.New(h))
+	seg, err := os.Open("testdata/paramount-avc1/seg_1.m4s")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer seg.Close()
+	var f File
+	if err := f.Read(seg); err != nil {
+		t.Fatal(err)
+	}
 }

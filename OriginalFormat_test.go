7ccc3bbc67785ea6e68839edb8b9f1f5576ac2a7
3052 <spen+3052@protonmail.com>
2024-03-19 21:27:29 -0500

    modified:   File.go

--- OriginalFormat_test.go
+++ OriginalFormat_test.go
@@ -1,27 +1,27 @@
 package sofia
 
 import (
-	"fmt"
-	"log/slog"
-	"os"
-	"testing"
+   "fmt"
+   "log/slog"
+   "os"
+   "testing"
 )
 
 func TestOriginalFormat(t *testing.T) {
-	h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	})
-	slog.SetDefault(slog.New(h))
-	media, err := os.Open("testdata/hulu-ec-3/init.mp4")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer media.Close()
-	var f File
-	if err := f.Read(media); err != nil {
-		t.Fatal(err)
-	}
-	format := f.Movie.Track.Media.MediaInformation.SampleTable.SampleDescription.
-		AudioSample.ProtectionScheme.OriginalFormat
-	fmt.Printf("%q\n", format.DataFormat)
+   h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+      Level: slog.LevelDebug,
+   })
+   slog.SetDefault(slog.New(h))
+   media, err := os.Open("testdata/hulu-ec-3/init.mp4")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer media.Close()
+   var f File
+   if err := f.Read(media); err != nil {
+      t.Fatal(err)
+   }
+   format := f.Movie.Track.Media.MediaInformation.SampleTable.SampleDescription.
+      AudioSample.ProtectionScheme.OriginalFormat
+   fmt.Printf("%q\n", format.DataFormat)
 }

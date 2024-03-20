8704b70ecdc45c6d1d27218bb80d5c70ec9ff7ab
3052 <spen+3052@protonmail.com>
2024-03-19 21:27:29 -0500

    modified:   File.go

--- File_test.go
+++ File_test.go
@@ -1,22 +1,22 @@
 package sofia
 
 import (
-	"os"
-	"testing"
+   "os"
+   "testing"
 )
 
 func TestFile(t *testing.T) {
-	for _, test := range tests {
-		func() {
-			src, err := os.Open(test.init)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer src.Close()
-			var dst File
-			if err := dst.Read(src); err != nil {
-				t.Fatal(err)
-			}
-		}()
-	}
+   for _, test := range tests {
+      func() {
+         src, err := os.Open(test.init)
+         if err != nil {
+            t.Fatal(err)
+         }
+         defer src.Close()
+         var dst File
+         if err := dst.Read(src); err != nil {
+            t.Fatal(err)
+         }
+      }()
+   }
 }

6e50c6980bcf911545a6e6ba509ed6bb5c9fcf87
3052 <spen+3052@protonmail.com>
2024-03-23 17:35:22 -0500

    +func (p *ProtectionSystemSpecificHeader) read(r io.Reader) error {

--- SegmentIndex.go
+++ SegmentIndex.go
@@ -43,6 +43,41 @@ func (r Reference) write(dst io.Writer) error {
    return binary.Write(dst, binary.BigEndian, r)
 }
 
+// ISO/IEC 14496-12
+//
+//   aligned(8) class SegmentIndexBox extends FullBox('sidx', version, 0) {
+//      unsigned int(32) reference_ID;
+//      unsigned int(32) timescale;
+//      if (version==0) {
+//         unsigned int(32) earliest_presentation_time;
+//         unsigned int(32) first_offset;
+//      } else {
+//         unsigned int(64) earliest_presentation_time;
+//         unsigned int(64) first_offset;
+//      }
+//      unsigned int(16) reserved = 0;
+//      unsigned int(16) reference_count;
+//      for(i=1; i <= reference_count; i++) {
+//         bit (1) reference_type;
+//         unsigned int(31) referenced_size;
+//         unsigned int(32) subsegment_duration;
+//         bit(1) starts_with_SAP;
+//         unsigned int(3) SAP_type;
+//         unsigned int(28) SAP_delta_time;
+//      }
+//   }
+type SegmentIndex struct {
+   BoxHeader                BoxHeader
+   FullBoxHeader            FullBoxHeader
+   ReferenceId              uint32
+   Timescale                uint32
+   EarliestPresentationTime []byte
+   FirstOffset              []byte
+   Reserved                 uint16
+   ReferenceCount           uint16
+   Reference                []Reference
+}
+
 func (s *SegmentIndex) Append(size uint32) {
    var r Reference
    r.set_referenced_size(size)
@@ -152,39 +187,4 @@ func (s SegmentIndex) write(w io.Writer) error {
       }
    }
    return nil
-}
-
-// ISO/IEC 14496-12
-//
-//   aligned(8) class SegmentIndexBox extends FullBox('sidx', version, 0) {
-//      unsigned int(32) reference_ID;
-//      unsigned int(32) timescale;
-//      if (version==0) {
-//         unsigned int(32) earliest_presentation_time;
-//         unsigned int(32) first_offset;
-//      } else {
-//         unsigned int(64) earliest_presentation_time;
-//         unsigned int(64) first_offset;
-//      }
-//      unsigned int(16) reserved = 0;
-//      unsigned int(16) reference_count;
-//      for(i=1; i <= reference_count; i++) {
-//         bit (1) reference_type;
-//         unsigned int(31) referenced_size;
-//         unsigned int(32) subsegment_duration;
-//         bit(1) starts_with_SAP;
-//         unsigned int(3) SAP_type;
-//         unsigned int(28) SAP_delta_time;
-//      }
-//   }
-type SegmentIndex struct {
-   BoxHeader                BoxHeader
-   FullBoxHeader            FullBoxHeader
-   ReferenceId              uint32
-   Timescale                uint32
-   EarliestPresentationTime []byte
-   FirstOffset              []byte
-   Reserved                 uint16
-   ReferenceCount           uint16
-   Reference                []Reference
 }

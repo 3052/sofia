3beaec933174210d080280bd7e976c37fdbdc80e
3052 <spen+3052@protonmail.com>
2024-03-23 11:26:47 -0500

    modified:   TrackRun.go

--- TrackRun.go
+++ TrackRun.go
@@ -5,67 +5,6 @@ import (
    "io"
 )
 
-// ISO/IEC 14496-12
-//
-// If the data-offset is present, it is relative to the base-data-offset
-// established in the track fragment header.
-//
-//   aligned(8) class TrackRunBox extends FullBox('trun', version, tr_flags) {
-//      unsigned int(32) sample_count;
-//      signed int(32) data_offset; // 0x000001, assume present
-//      unsigned int(32) first_sample_flags; // 0x000004
-//      {
-//         unsigned int(32) sample_duration; // 0x000100
-//         unsigned int(32) sample_size; // 0x000200, assume present
-//         unsigned int(32) sample_flags // 0x000400
-//         if (version == 0) {
-//            unsigned int(32) sample_composition_time_offset; // 0x000800
-//         } else {
-//            signed int(32) sample_composition_time_offset; // 0x000800
-//         }
-//      }[ sample_count ]
-//   }
-type TrackRun struct {
-   BoxHeader        BoxHeader
-   FullBoxHeader    FullBoxHeader
-   SampleCount      uint32
-   DataOffset       int32
-   FirstSampleFlags uint32
-   Sample           []RunSample
-}
-
-type RunSample struct {
-   Duration              uint32
-   Size                  uint32
-   Flags                 uint32
-   CompositionTimeOffset [4]byte
-}
-
-// 0x000004 first-sample-flags-present
-func (t TrackRun) first_sample_flags_present() bool {
-   return t.FullBoxHeader.get_flags()&4 >= 1
-}
-
-// 0x000800 sample-composition-time-offsets-present
-func (t TrackRun) sample_composition_time_offsets_present() bool {
-   return t.FullBoxHeader.get_flags()&0x800 >= 1
-}
-
-// 0x000100 sample-duration-present
-func (t TrackRun) sample_duration_present() bool {
-   return t.FullBoxHeader.get_flags()&0x100 >= 1
-}
-
-// 0x000400 sample-flags-present
-func (t TrackRun) sample_flags_present() bool {
-   return t.FullBoxHeader.get_flags()&0x400 >= 1
-}
-
-// 0x000200 sample-size-present
-func (t TrackRun) sample_size_present() bool {
-   return t.FullBoxHeader.get_flags()&0x200 >= 1
-}
-
 func (s RunSample) write(w io.Writer, t TrackRun) error {
    if t.sample_duration_present() {
       err := binary.Write(w, binary.BigEndian, s.Duration)
@@ -180,3 +119,64 @@ func (t *TrackRun) read(r io.Reader) error {
    }
    return nil
 }
+// ISO/IEC 14496-12
+//
+// If the data-offset is present, it is relative to the base-data-offset
+// established in the track fragment header.
+//
+//   aligned(8) class TrackRunBox extends FullBox('trun', version, tr_flags) {
+//      unsigned int(32) sample_count;
+//      signed int(32) data_offset; // 0x000001, assume present
+//      unsigned int(32) first_sample_flags; // 0x000004
+//      {
+//         unsigned int(32) sample_duration; // 0x000100
+//         unsigned int(32) sample_size; // 0x000200, assume present
+//         unsigned int(32) sample_flags // 0x000400
+//         if (version == 0) {
+//            unsigned int(32) sample_composition_time_offset; // 0x000800
+//         } else {
+//            signed int(32) sample_composition_time_offset; // 0x000800
+//         }
+//      }[ sample_count ]
+//   }
+type TrackRun struct {
+   BoxHeader        BoxHeader
+   FullBoxHeader    FullBoxHeader
+   SampleCount      uint32
+   DataOffset       int32
+   FirstSampleFlags uint32
+   Sample           []RunSample
+}
+
+type RunSample struct {
+   Duration              uint32
+   Size                  uint32
+   Flags                 uint32
+   CompositionTimeOffset [4]byte
+}
+
+// 0x000004 first-sample-flags-present
+func (t TrackRun) first_sample_flags_present() bool {
+   return t.FullBoxHeader.get_flags()&4 >= 1
+}
+
+// 0x000800 sample-composition-time-offsets-present
+func (t TrackRun) sample_composition_time_offsets_present() bool {
+   return t.FullBoxHeader.get_flags()&0x800 >= 1
+}
+
+// 0x000100 sample-duration-present
+func (t TrackRun) sample_duration_present() bool {
+   return t.FullBoxHeader.get_flags()&0x100 >= 1
+}
+
+// 0x000400 sample-flags-present
+func (t TrackRun) sample_flags_present() bool {
+   return t.FullBoxHeader.get_flags()&0x400 >= 1
+}
+
+// 0x000200 sample-size-present
+func (t TrackRun) sample_size_present() bool {
+   return t.FullBoxHeader.get_flags()&0x200 >= 1
+}
+

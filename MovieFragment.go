00409360c403bbb2c1a705581668da2057912b2a
3052 <spen+3052@protonmail.com>
2024-03-23 11:49:42 -0500

    +func (e *EncryptionSample) read(r io.Reader, box *SampleEncryption) error {

--- MovieFragment.go
+++ MovieFragment.go
@@ -16,20 +16,6 @@ type MovieFragment struct {
    TrackFragment TrackFragment
 }
 
-func (m MovieFragment) write(w io.Writer) error {
-   err := m.BoxHeader.write(w)
-   if err != nil {
-      return err
-   }
-   for _, b := range m.Boxes {
-      err := b.write(w)
-      if err != nil {
-         return err
-      }
-   }
-   return m.TrackFragment.write(w)
-}
-
 func (m *MovieFragment) read(r io.Reader, size int64) error {
    r = io.LimitReader(r, size)
    for {
@@ -62,4 +48,18 @@ func (m *MovieFragment) read(r io.Reader, size int64) error {
          return errors.New("MovieFragment.read")
       }
    }
+}
+
+func (m MovieFragment) write(w io.Writer) error {
+   err := m.BoxHeader.write(w)
+   if err != nil {
+      return err
+   }
+   for _, b := range m.Boxes {
+      err := b.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   return m.TrackFragment.write(w)
 }

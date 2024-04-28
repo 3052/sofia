9043c9ea3cc44875b81d04832596a55fbbb7cfbb
3052 <spen+3052@protonmail.com>
2024-04-27 21:54:02 -0500

    +func (m Movie) Widevine() ([]uint8, bool) {

--- Movie.go
+++ Movie.go
@@ -5,12 +5,23 @@ import (
    "io"
 )
 
+// dashif.org/identifiers/content_protection
+func (m Movie) Widevine() ([]uint8, bool) {
+   for _, protect := range m.Protection {
+      if protect.SystemId.String() == "edef8ba979d64acea3c827dcd51d21ed" {
+         return protect.Data, true
+      }
+   }
+   return nil, false
+}
+
 // ISO/IEC 14496-12
 //  aligned(8) class MovieBox extends Box('moov') {
 //  }
 type Movie struct {
    BoxHeader BoxHeader
    Boxes     []*Box
+   Protection []ProtectionSystemSpecificHeader
    Track     Track
 }
 
@@ -28,14 +39,20 @@ func (m *Movie) read(r io.Reader, size int64) error {
       case "iods", // Roku
       "meta", // Paramount
       "mvex", // Roku
-      "mvhd", // Roku
-      "pssh": // Stan
-         value := Box{BoxHeader: head}
-         err := value.read(r)
+      "mvhd": // Roku
+         object := Box{BoxHeader: head}
+         err := object.read(r)
          if err != nil {
             return err
          }
-         m.Boxes = append(m.Boxes, &value)
+         m.Boxes = append(m.Boxes, &object)
+      case "pssh":
+         protect := ProtectionSystemSpecificHeader{BoxHeader: head}
+         err := protect.read(r)
+         if err != nil {
+            return err
+         }
+         m.Protection = append(m.Protection, protect)
       case "trak":
          _, size := head.get_size()
          m.Track.BoxHeader = head
@@ -54,8 +71,14 @@ func (m Movie) write(w io.Writer) error {
    if err != nil {
       return err
    }
-   for _, value := range m.Boxes {
-      err := value.write(w)
+   for _, object := range m.Boxes {
+      err := object.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   for _, protect := range m.Protection {
+      err := protect.write(w)
       if err != nil {
          return err
       }

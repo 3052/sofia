c6eef42ea10c821796357fc332b4d834d26d24d8
3052 <spen+3052@protonmail.com>
2024-05-06 17:25:53 -0500

    new file:   testdata/draken/init.mp4

--- File_test.go
+++ File_test.go
@@ -1,23 +1,21 @@
 package sofia
 
 import (
+   "fmt"
    "os"
    "testing"
 )
 
 func TestFile(t *testing.T) {
-   for _, test := range tests {
-      func() {
-         src, err := os.Open(test.init)
-         if err != nil {
-            t.Fatal(err)
-         }
-         defer src.Close()
-         var dst File
-         err = dst.Read(src)
-         if err != nil {
-            t.Fatal(err)
-         }
-      }()
+   from, err := os.Open("testdata/draken/init.mp4")
+   if err != nil {
+      t.Fatal(err)
    }
+   defer from.Close()
+   var to File
+   err = to.Read(from)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", to.Movie.Protection)
 }

17931e1fc21a053a9836e6ae590dd1457e0625f8
3052 <spen+3052@protonmail.com>
2024-06-09 12:29:50 -0500

    new file:   testdata/rtbf/vod-idx-video=4000000.dash.txt

--- File_test.go
+++ File_test.go
@@ -1,17 +1,19 @@
 package sofia
 
 import (
+   "log/slog"
    "os"
    "testing"
 )
 
-func TestCine(t *testing.T) {
-   in, err := os.Open("testdata/cine-member/video_eng=110000.dash")
+func TestFile(t *testing.T) {
+   in, err := os.Open("testdata/rtbf/vod-idx-video=4000000.dash")
    if err != nil {
       t.Fatal(err)
    }
    defer in.Close()
    var out File
+   slog.SetLogLoggerLevel(slog.LevelDebug)
    err = out.Read(in)
    if err != nil {
       t.Fatal(err)

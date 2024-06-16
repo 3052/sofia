b3099e9ce882076cf46f601fbd364dfc2a2d75de
3052 <spen+3052@protonmail.com>
2024-06-16 16:42:15 -0500

    modified:   SampleTable.go

--- SampleTable.go
+++ SampleTable.go
@@ -1,66 +1,65 @@
 package sofia
 
 import (
-	"errors"
-	"io"
+   "errors"
+   "io"
 )
 
 // ISO/IEC 14496-12
-//
-//	aligned(8) class SampleTableBox extends Box('stbl') {
-//	}
+//   aligned(8) class SampleTableBox extends Box('stbl') {
+//   }
 type SampleTable struct {
-	BoxHeader         BoxHeader
-	Boxes             []Box
-	SampleDescription SampleDescription
+   BoxHeader         BoxHeader
+   Boxes             []Box
+   SampleDescription SampleDescription
 }
 
 func (s *SampleTable) read(r io.Reader, size int64) error {
-	r = io.LimitReader(r, size)
-	for {
-		var head BoxHeader
-		err := head.Read(r)
-		if err == io.EOF {
-			return nil
-		} else if err != nil {
-			return err
-		}
-		switch head.debug() {
-		case "stsd":
-			_, size := head.get_size()
-			s.SampleDescription.BoxHeader = head
-			err := s.SampleDescription.read(r, size)
-			if err != nil {
-				return err
-			}
-		case "sgpd", // Paramount
-			"stco", // Roku
-			"stsc", // Roku
-			"stss", // CineMember
-			"stsz", // Roku
-			"stts": // Roku
-			object := Box{BoxHeader: head}
-			err := object.read(r)
-			if err != nil {
-				return err
-			}
-			s.Boxes = append(s.Boxes, object)
-		default:
-			return errors.New("SampleTable.read")
-		}
-	}
+   r = io.LimitReader(r, size)
+   for {
+      var head BoxHeader
+      err := head.Read(r)
+      if err == io.EOF {
+         return nil
+      } else if err != nil {
+         return err
+      }
+      switch head.debug() {
+      case "stsd":
+         _, size := head.get_size()
+         s.SampleDescription.BoxHeader = head
+         err := s.SampleDescription.read(r, size)
+         if err != nil {
+            return err
+         }
+      case "sgpd", // Paramount
+         "stco", // Roku
+         "stsc", // Roku
+         "stss", // CineMember
+         "stsz", // Roku
+         "stts": // Roku
+         object := Box{BoxHeader: head}
+         err := object.read(r)
+         if err != nil {
+            return err
+         }
+         s.Boxes = append(s.Boxes, object)
+      default:
+         return errors.New("SampleTable.read")
+      }
+   }
 }
 
 func (s SampleTable) write(w io.Writer) error {
-	err := s.BoxHeader.write(w)
-	if err != nil {
-		return err
-	}
-	for _, object := range s.Boxes {
-		err := object.write(w)
-		if err != nil {
-			return err
-		}
-	}
-	return s.SampleDescription.write(w)
+   err := s.BoxHeader.write(w)
+   if err != nil {
+      return err
+   }
+   for _, object := range s.Boxes {
+      err := object.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   return s.SampleDescription.write(w)
 }

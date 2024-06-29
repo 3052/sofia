fb1efc1440fd5f9234a12112a079d0c9ac6e6921
3052 <spen+3052@protonmail.com>
2024-06-29 18:36:12 -0500

    +      "testdata/max/init.mp4",

--- File.go
+++ File.go
@@ -1,108 +1,108 @@
 package sofia
 
 import (
-	"errors"
-	"io"
+   "errors"
+   "io"
 )
 
 // ISO/IEC 14496-12
 type File struct {
-	Boxes         []Box
-	MediaData     *MediaData
-	Movie         *Movie
-	MovieFragment *MovieFragment
-	SegmentIndex  *SegmentIndex
+   Boxes         []Box
+   MediaData     *MediaData
+   Movie         *Movie
+   MovieFragment *MovieFragment
+   SegmentIndex  *SegmentIndex
 }
 
 func (f File) GetMovie() (*Movie, bool) {
-	if v := f.Movie; v != nil {
-		return v, true
-	}
-	return nil, false
+   if v := f.Movie; v != nil {
+      return v, true
+   }
+   return nil, false
 }
 
 func (f *File) Read(r io.Reader) error {
-	for {
-		var head BoxHeader
-		err := head.Read(r)
-		if err == io.EOF {
-			return nil
-		} else if err != nil {
-			return err
-		}
-		_, size := head.get_size()
-		switch head.debug() {
-		case "mdat":
-			f.MediaData = new(MediaData)
-			f.MediaData.Box.BoxHeader = head
-			err := f.MediaData.read(r)
-			if err != nil {
-				return err
-			}
-		case "moof":
-			f.MovieFragment = &MovieFragment{BoxHeader: head}
-			err := f.MovieFragment.read(r, size)
-			if err != nil {
-				return err
-			}
-		case "sidx":
-			f.SegmentIndex = &SegmentIndex{BoxHeader: head}
-			err := f.SegmentIndex.read(r)
-			if err != nil {
-				return err
-			}
-		case "moov":
-			f.Movie = &Movie{BoxHeader: head}
-			err := f.Movie.read(r, size)
-			if err != nil {
-				return err
-			}
-		case "free", // Mubi
-			"ftyp", // Roku
-			"styp": // Roku
-			object := Box{BoxHeader: head}
-			err := object.read(r)
-			if err != nil {
-				return err
-			}
-			f.Boxes = append(f.Boxes, object)
-		default:
-			return errors.New("File.Read")
-		}
-	}
+   for {
+      var head BoxHeader
+      err := head.Read(r)
+      if err == io.EOF {
+         return nil
+      } else if err != nil {
+         return err
+      }
+      _, size := head.get_size()
+      switch head.debug() {
+      case "mdat":
+         f.MediaData = new(MediaData)
+         f.MediaData.Box.BoxHeader = head
+         err := f.MediaData.read(r)
+         if err != nil {
+            return err
+         }
+      case "moof":
+         f.MovieFragment = &MovieFragment{BoxHeader: head}
+         err := f.MovieFragment.read(r, size)
+         if err != nil {
+            return err
+         }
+      case "sidx":
+         f.SegmentIndex = &SegmentIndex{BoxHeader: head}
+         err := f.SegmentIndex.read(r)
+         if err != nil {
+            return err
+         }
+      case "moov":
+         f.Movie = &Movie{BoxHeader: head}
+         err := f.Movie.read(r, size)
+         if err != nil {
+            return err
+         }
+      case "free", // Mubi
+         "ftyp", // Roku
+         "styp": // Roku
+         object := Box{BoxHeader: head}
+         err := object.read(r)
+         if err != nil {
+            return err
+         }
+         f.Boxes = append(f.Boxes, object)
+      default:
+         return errors.New("File.Read")
+      }
+   }
 }
 
 func (f File) Write(w io.Writer) error {
-	// KEEP THESE IN ORDER
-	for _, value := range f.Boxes {
-		err := value.write(w)
-		if err != nil {
-			return err
-		}
-	}
-	if f.Movie != nil { // moov
-		err := f.Movie.write(w)
-		if err != nil {
-			return err
-		}
-	}
-	if f.SegmentIndex != nil { // sidx
-		err := f.SegmentIndex.write(w)
-		if err != nil {
-			return err
-		}
-	}
-	if f.MovieFragment != nil { // moof
-		err := f.MovieFragment.write(w)
-		if err != nil {
-			return err
-		}
-	}
-	if f.MediaData != nil { // mdat
-		err := f.MediaData.write(w)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+   // KEEP THESE IN ORDER
+   for _, value := range f.Boxes {
+      err := value.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   if f.Movie != nil { // moov
+      err := f.Movie.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   if f.SegmentIndex != nil { // sidx
+      err := f.SegmentIndex.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   if f.MovieFragment != nil { // moof
+      err := f.MovieFragment.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   if f.MediaData != nil { // mdat
+      err := f.MediaData.write(w)
+      if err != nil {
+         return err
+      }
+   }
+   return nil
 }

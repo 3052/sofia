275671b55285e2778d2b4566b8185514f4645d8a
3052 <spen+3052@protonmail.com>
2024-09-07 18:15:12 -0500

    modified:   box/box.go

--- Media.go
+++ Media.go
@@ -1,64 +1,64 @@
 package sofia
 
 import (
-	"154.pages.dev/sofia/box"
-	"io"
+   "154.pages.dev/sofia/box"
+   "io"
 )
 
 func (m *Media) read(r io.Reader, size int64) error {
-	r = io.LimitReader(r, size)
-	for {
-		var head box.Header
-		err := head.Read(r)
-		switch err {
-		case nil:
-			switch head.Type.String() {
-			case "minf":
-				_, size := head.get_size()
-				m.MediaInformation.BoxHeader = head
-				err := m.MediaInformation.read(r, size)
-				if err != nil {
-					return err
-				}
-			case "hdlr", // Roku
-				"mdhd": // Roku
-				object := box.Box{BoxHeader: head}
-				err := object.Read(r)
-				if err != nil {
-					return err
-				}
-				m.Boxes = append(m.Boxes, object)
-			default:
-				return box.Error{m.BoxHeader.Type, head.Type}
-			}
-		case io.EOF:
-			return nil
-		default:
-			return err
-		}
-	}
+   r = io.LimitReader(r, size)
+   for {
+      var head box.Header
+      err := head.Read(r)
+      switch err {
+      case nil:
+         switch head.Type.String() {
+         case "minf":
+            _, size := head.GetSize()
+            m.MediaInformation.BoxHeader = head
+            err := m.MediaInformation.read(r, size)
+            if err != nil {
+               return err
+            }
+         case "hdlr", // Roku
+            "mdhd": // Roku
+            object := box.Box{BoxHeader: head}
+            err := object.Read(r)
+            if err != nil {
+               return err
+            }
+            m.Boxes = append(m.Boxes, object)
+         default:
+            return box.Error{m.BoxHeader.Type, head.Type}
+         }
+      case io.EOF:
+         return nil
+      default:
+         return err
+      }
+   }
 }
 
 // ISO/IEC 14496-12
 //
-//	aligned(8) class MediaBox extends Box('mdia') {
-//	}
+//   aligned(8) class MediaBox extends Box('mdia') {
+//   }
 type Media struct {
-	BoxHeader        box.Header
-	Boxes            []box.Box
-	MediaInformation MediaInformation
+   BoxHeader        box.Header
+   Boxes            []box.Box
+   MediaInformation MediaInformation
 }
 
 func (m *Media) write(w io.Writer) error {
-	err := m.BoxHeader.write(w)
-	if err != nil {
-		return err
-	}
-	for _, object := range m.Boxes {
-		err := object.Write(w)
-		if err != nil {
-			return err
-		}
-	}
-	return m.MediaInformation.write(w)
+   err := m.BoxHeader.Write(w)
+   if err != nil {
+      return err
+   }
+   for _, object := range m.Boxes {
+      err := object.Write(w)
+      if err != nil {
+         return err
+      }
+   }
+   return m.MediaInformation.write(w)
 }

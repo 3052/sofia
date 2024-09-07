2db447fa090ff2baf3a5793c7ed8e20b3b931422
3052 <spen+3052@protonmail.com>
2024-09-07 18:58:53 -0500

    modified:   Movie.go

--- MovieFragment.go
+++ MovieFragment.go
@@ -1,64 +1,64 @@
 package sofia
 
 import (
-	"154.pages.dev/sofia/box"
-	"io"
+   "154.pages.dev/sofia/box"
+   "io"
 )
 
 func (m *MovieFragment) read(r io.Reader, size int64) error {
-	r = io.LimitReader(r, size)
-	for {
-		var head box.Header
-		err := head.Read(r)
-		switch err {
-		case nil:
-			switch head.Type.String() {
-			case "traf":
-				_, size := head.GetSize()
-				m.TrackFragment.BoxHeader = head
-				err := m.TrackFragment.read(r, size)
-				if err != nil {
-					return err
-				}
-			case "mfhd", // Roku
-				"pssh": // Roku
-				object := box.Box{BoxHeader: head}
-				err := object.read(r)
-				if err != nil {
-					return err
-				}
-				m.Boxes = append(m.Boxes, object)
-			default:
-				return box.Error{m.BoxHeader.Type, head.Type}
-			}
-		case io.EOF:
-			return nil
-		default:
-			return err
-		}
-	}
+   r = io.LimitReader(r, size)
+   for {
+      var head box.Header
+      err := head.Read(r)
+      switch err {
+      case nil:
+         switch head.Type.String() {
+         case "traf":
+            _, size := head.GetSize()
+            m.TrackFragment.BoxHeader = head
+            err := m.TrackFragment.read(r, size)
+            if err != nil {
+               return err
+            }
+         case "mfhd", // Roku
+            "pssh": // Roku
+            value := box.Box{BoxHeader: head}
+            err := value.read(r)
+            if err != nil {
+               return err
+            }
+            m.Boxes = append(m.Boxes, value)
+         default:
+            return box.Error{m.BoxHeader.Type, head.Type}
+         }
+      case io.EOF:
+         return nil
+      default:
+         return err
+      }
+   }
 }
 
 // ISO/IEC 14496-12
 //
-//	aligned(8) class MovieFragmentBox extends Box('moof') {
-//	}
+//   aligned(8) class MovieFragmentBox extends Box('moof') {
+//   }
 type MovieFragment struct {
-	BoxHeader     box.Header
-	Boxes         []box.Box
-	TrackFragment TrackFragment
+   BoxHeader     box.Header
+   Boxes         []box.Box
+   TrackFragment TrackFragment
 }
 
 func (m MovieFragment) write(w io.Writer) error {
-	err := m.BoxHeader.write(w)
-	if err != nil {
-		return err
-	}
-	for _, object := range m.Boxes {
-		err := object.write(w)
-		if err != nil {
-			return err
-		}
-	}
-	return m.TrackFragment.write(w)
+   err := m.BoxHeader.Write(w)
+   if err != nil {
+      return err
+   }
+   for _, value := range m.Boxes {
+      err := value.Write(w)
+      if err != nil {
+         return err
+      }
+   }
+   return m.TrackFragment.write(w)
 }

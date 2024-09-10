547b514d26bc12d3d6b00340fb31ede0c12e4c99
3052 <spen+3052@protonmail.com>
2024-09-09 21:45:49 -0500

    new file:   testdata/paramount-avc1/0-1925.mp4

--- file/senc_test.go
+++ file/senc_test.go
@@ -8,144 +8,36 @@ import (
    "testing"
 )
 
-func (t testdata) encode_segment(write io.Writer) error {
-   fmt.Println(t.segment)
-   in, err := os.Open(t.segment)
+func (s senc_test) encode_segment(dst io.Writer) error {
+   fmt.Println(s.segment)
+   src, err := os.Open(s.segment)
    if err != nil {
       return err
    }
-   defer in.Close()
-   var out File
-   err = out.Read(in)
+   defer src.Close()
+   var value File
+   err = value.Read(src)
    if err != nil {
       return err
    }
-   track := out.MovieFragment.TrackFragment
+   track := value.MovieFragment.TrackFragment
    if encrypt := track.SampleEncryption; encrypt != nil {
-      key, err := hex.DecodeString(t.key)
+      key, err := hex.DecodeString(s.key)
       if err != nil {
          return err
       }
-      for i, text := range out.MediaData.Data(track) {
+      for i, text := range value.MediaData.Data(track) {
          err := encrypt.Sample[i].DecryptCenc(text, key)
          if err != nil {
             return err
          }
       }
    }
-   return out.Write(write)
+   return value.Write(dst)
 }
 
-func TestSenc(t *testing.T) {
-   for _, test := range tests {
-      func() {
-         out, err := os.Create(test.out)
-         if err != nil {
-            t.Fatal(err)
-         }
-         defer out.Close()
-         err = test.encode_init(out)
-         if err != nil {
-            t.Fatal(err)
-         }
-         err = test.encode_segment(out)
-         if err != nil {
-            t.Fatal(err)
-         }
-      }()
-   }
-}
-
-var tests = []testdata{
-   {
-      "testdata/amc-avc1/init.m4f",
-      "testdata/amc-avc1/segment0.m4f",
-      "c58d3308ed18d43776a78232f552dbe0",
-      "amc-avc1.mp4",
-   },
-   {
-      "testdata/amc-mp4a/init.m4f",
-      "testdata/amc-mp4a/segment0.m4f",
-      "91d888dfb0562ebc3abdd845d451e858",
-      "amc-mp4a.mp4",
-   },
-   {
-      "testdata/hulu-avc1/init.mp4",
-      "testdata/hulu-avc1/segment-1.0001.m4s",
-      "602a9289bfb9b1995b75ac63f123fc86",
-      "hulu-avc1.mp4",
-   },
-   {
-      "testdata/hulu-ec-3/init.mp4",
-      "testdata/hulu-ec-3/segment-1.0001.m4s",
-      "7be76f0d9c8a0db0b7f6059bf0a1c023",
-      "hulu-ec-3.mp4",
-   },
-   {
-      "testdata/hulu-mp4a/init.mp4",
-      "testdata/hulu-mp4a/segment-1.0001.m4s",
-      "602a9289bfb9b1995b75ac63f123fc86",
-      "hulu-mp4a.mp4",
-   },
-   {
-      "testdata/mubi-avc1/video=300168.dash",
-      "testdata/mubi-avc1/video=300168-0.dash",
-      "2556f746e8db3ee7f66fc22f5a28752a",
-      "mubi-avc1.mp4",
-   },
-   {
-      "testdata/mubi-mp4a/audio_eng=268840.dash",
-      "testdata/mubi-mp4a/audio_eng=268840-0.dash",
-      "2556f746e8db3ee7f66fc22f5a28752a",
-      "mubi-mp4a.mp4",
-   },
-   {
-      "testdata/nbc-avc1/_227156876_5.mp4",
-      "testdata/nbc-avc1/_227156876_5_0.mp4",
-      "3e2e8ccff89d0a72598a347feab5e7c8",
-      "nbc-avc1.mp4",
-   },
-   {
-      "testdata/nbc-mp4a/_227156876_6_1.mp4",
-      "testdata/nbc-mp4a/_227156876_6_1_0.mp4",
-      "3e2e8ccff89d0a72598a347feab5e7c8",
-      "nbc-mp4a.mp4",
-   },
-   {
-      "testdata/paramount-avc1/init.m4v",
-      "testdata/paramount-avc1/seg_1.m4s",
-      "efa0258cafde6102f513f031d0632290",
-      "paramount-avc1.mp4",
-   },
-   {
-      "testdata/paramount-mp4a/init.m4v",
-      "testdata/paramount-mp4a/seg_1.m4s",
-      "d98277ff6d7406ec398b49bbd52937d4",
-      "paramount-mp4a.mp4",
-   },
-   {
-      "testdata/roku-avc1/index_video_8_0_init.mp4",
-      "testdata/roku-avc1/index_video_8_0_1.mp4",
-      "1ba08384626f9523e37b9db17f44da2b",
-      "roku-avc1.mp4",
-   },
-   {
-      "testdata/roku-mp4a/index_audio_2_0_init.mp4",
-      "testdata/roku-mp4a/index_audio_2_0_1.mp4",
-      "1ba08384626f9523e37b9db17f44da2b",
-      "roku-mp4a.mp4",
-   },
-}
-
-type testdata struct {
-   init    string
-   segment string
-   key     string
-   out     string
-}
-
-func (t testdata) encode_init(dst io.Writer) error {
-   src, err := os.Open(t.init)
+func (s senc_test) encode_init(dst io.Writer) error {
+   src, err := os.Open(s.init)
    if err != nil {
       return err
    }
@@ -173,4 +65,112 @@ func (t testdata) encode_init(dst io.Writer) error {
       }
    }
    return value.Write(dst)
+}
+
+type senc_test struct {
+   init    string
+   segment string
+   key     string
+   dst     string
+}
+
+var senc_tests = []senc_test{
+   {
+      "../testdata/amc-avc1/init.m4f",
+      "../testdata/amc-avc1/segment0.m4f",
+      "c58d3308ed18d43776a78232f552dbe0",
+      "amc-avc1.mp4",
+   },
+   {
+      "../testdata/amc-mp4a/init.m4f",
+      "../testdata/amc-mp4a/segment0.m4f",
+      "91d888dfb0562ebc3abdd845d451e858",
+      "amc-mp4a.mp4",
+   },
+   {
+      "../testdata/hulu-avc1/init.mp4",
+      "../testdata/hulu-avc1/segment-1.0001.m4s",
+      "602a9289bfb9b1995b75ac63f123fc86",
+      "hulu-avc1.mp4",
+   },
+   {
+      "../testdata/hulu-ec-3/init.mp4",
+      "../testdata/hulu-ec-3/segment-1.0001.m4s",
+      "7be76f0d9c8a0db0b7f6059bf0a1c023",
+      "hulu-ec-3.mp4",
+   },
+   {
+      "../testdata/hulu-mp4a/init.mp4",
+      "../testdata/hulu-mp4a/segment-1.0001.m4s",
+      "602a9289bfb9b1995b75ac63f123fc86",
+      "hulu-mp4a.mp4",
+   },
+   {
+      "../testdata/mubi-avc1/video=300168.dash",
+      "../testdata/mubi-avc1/video=300168-0.dash",
+      "2556f746e8db3ee7f66fc22f5a28752a",
+      "mubi-avc1.mp4",
+   },
+   {
+      "../testdata/mubi-mp4a/audio_eng=268840.dash",
+      "../testdata/mubi-mp4a/audio_eng=268840-0.dash",
+      "2556f746e8db3ee7f66fc22f5a28752a",
+      "mubi-mp4a.mp4",
+   },
+   {
+      "../testdata/nbc-avc1/_227156876_5.mp4",
+      "../testdata/nbc-avc1/_227156876_5_0.mp4",
+      "3e2e8ccff89d0a72598a347feab5e7c8",
+      "nbc-avc1.mp4",
+   },
+   {
+      "../testdata/nbc-mp4a/_227156876_6_1.mp4",
+      "../testdata/nbc-mp4a/_227156876_6_1_0.mp4",
+      "3e2e8ccff89d0a72598a347feab5e7c8",
+      "nbc-mp4a.mp4",
+   },
+   {
+      "../testdata/paramount-avc1/init.m4v",
+      "../testdata/paramount-avc1/seg_1.m4s",
+      "efa0258cafde6102f513f031d0632290",
+      "paramount-avc1.mp4",
+   },
+   {
+      "../testdata/paramount-mp4a/init.m4v",
+      "../testdata/paramount-mp4a/seg_1.m4s",
+      "d98277ff6d7406ec398b49bbd52937d4",
+      "paramount-mp4a.mp4",
+   },
+   {
+      "../testdata/roku-avc1/index_video_8_0_init.mp4",
+      "../testdata/roku-avc1/index_video_8_0_1.mp4",
+      "1ba08384626f9523e37b9db17f44da2b",
+      "roku-avc1.mp4",
+   },
+   {
+      "../testdata/roku-mp4a/index_audio_2_0_init.mp4",
+      "../testdata/roku-mp4a/index_audio_2_0_1.mp4",
+      "1ba08384626f9523e37b9db17f44da2b",
+      "roku-mp4a.mp4",
+   },
+}
+
+func TestSenc(t *testing.T) {
+   for _, test := range senc_tests {
+      func() {
+         dst, err := os.Create(test.dst)
+         if err != nil {
+            t.Fatal(err)
+         }
+         defer dst.Close()
+         err = test.encode_init(dst)
+         if err != nil {
+            t.Fatal(err)
+         }
+         err = test.encode_segment(dst)
+         if err != nil {
+            t.Fatal(err)
+         }
+      }()
+   }
 }
--- testdata/paramount-avc1/readme.md
+++ testdata/paramount-avc1/readme.md
@@ -1,6 +1,7 @@
 # paramount
 
 ~~~
-> paramount -b xzUce3_6ljEEGyIu9rCpndUPhDgfZF1X -i 2 -v debug
-msg=hex key=efa0258cafde6102f513f031d0632290
+paramount -b xzUce3_6ljEEGyIu9rCpndUPhDgfZF1X -w
+paramount -b xzUce3_6ljEEGyIu9rCpndUPhDgfZF1X
+paramount -b xzUce3_6ljEEGyIu9rCpndUPhDgfZF1X -i 19
 ~~~

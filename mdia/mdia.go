c4753f6bd0f2426ad44f81ac61dc7c4534a1fdba
3052 <spen+3052@protonmail.com>
2024-09-14 15:15:08 -0500

    deleted:    mdia/mdia_test.go

--- mdia/mdia.go
+++ mdia/mdia.go
@@ -3,8 +3,30 @@ package mdia
 import (
    "154.pages.dev/sofia"
    "154.pages.dev/sofia/minf"
-   "io"
 )
+
+// ISO/IEC 14496-12
+//   aligned(8) class MediaBox extends Box('mdia') {
+//   }
+type Box struct {
+   BoxHeader sofia.BoxHeader
+   Box       []sofia.Box
+   Minf      minf.Box
+}
+
+func (b *Box) Append(buf []byte) ([]byte, error) {
+   buf, err := b.BoxHeader.Append(buf)
+   if err != nil {
+      return nil, err
+   }
+   for _, sof := range b.Box {
+      buf, err = sof.Append(buf)
+      if err != nil {
+         return nil, err
+      }
+   }
+   return b.Minf.Append(buf)
+}
 
 func (b *Box) Read(src io.Reader, size int64) error {
    src = io.LimitReader(src, size)
@@ -22,12 +44,12 @@ func (b *Box) Read(src io.Reader, size int64) error {
             b.Minf.BoxHeader = head
          case "hdlr", // Roku
             "mdhd": // Roku
-            value := sofia.Box{BoxHeader: head}
-            err := value.Read(src)
+            sof := sofia.Box{BoxHeader: head}
+            err := sof.Read(src)
             if err != nil {
                return err
             }
-            b.Box = append(b.Box, value)
+            b.Box = append(b.Box, sof)
          default:
             return sofia.Error{b.BoxHeader.Type, head.Type}
          }
@@ -37,27 +59,4 @@ func (b *Box) Read(src io.Reader, size int64) error {
          return err
       }
    }
-}
-
-func (b *Box) Write(dst io.Writer) error {
-   err := b.BoxHeader.Write(dst)
-   if err != nil {
-      return err
-   }
-   for _, value := range b.Box {
-      err := value.Write(dst)
-      if err != nil {
-         return err
-      }
-   }
-   return b.Minf.Write(dst)
-}
-
-// ISO/IEC 14496-12
-//   aligned(8) class MediaBox extends Box('mdia') {
-//   }
-type Box struct {
-   BoxHeader sofia.BoxHeader
-   Box       []sofia.Box
-   Minf      minf.Box
 }
--- minf/minf.go
+++ minf/minf.go
@@ -3,11 +3,9 @@ package minf
 import (
    "154.pages.dev/sofia"
    "154.pages.dev/sofia/stbl"
-   "io"
 )
 
 // ISO/IEC 14496-12
-//
 //   aligned(8) class MediaInformationBox extends Box('minf') {
 //   }
 type Box struct {
@@ -16,50 +14,43 @@ type Box struct {
    Stbl      stbl.Box
 }
 
-func (b *Box) Read(src io.Reader, size int64) error {
-   src = io.LimitReader(src, size)
-   for {
-      var head sofia.BoxHeader
-      err := head.Read(src)
-      switch err {
-      case nil:
-         switch head.Type.String() {
-         case "stbl":
-            err := b.Stbl.Read(src, head.PayloadSize())
-            if err != nil {
-               return err
-            }
-            b.Stbl.BoxHeader = head
-         case "dinf", // Roku
-            "smhd", // Roku
-            "vmhd": // Roku
-            value := sofia.Box{BoxHeader: head}
-            err := value.Read(src)
-            if err != nil {
-               return err
-            }
-            b.Box = append(b.Box, value)
-         default:
-            return sofia.Error{b.BoxHeader.Type, head.Type}
-         }
-      case io.EOF:
-         return nil
-      default:
-         return err
+func (b *Box) Append(buf []byte) ([]byte, error) {
+   buf, err := b.BoxHeader.Append(buf)
+   if err != nil {
+      return nil, err
+   }
+   for _, sof := range b.Box {
+      buf, err = sof.Append(buf)
+      if err != nil {
+         return nil, err
       }
    }
+   return b.Stbl.Append(buf)
 }
 
-func (b *Box) Write(dst io.Writer) error {
-   err := b.BoxHeader.Write(dst)
-   if err != nil {
-      return err
-   }
-   for _, value := range b.Box {
-      err := value.Write(dst)
+func (b *Box) Decode(buf []byte) error {
+   for len(buf) >= 1 {
+      var sof sofia.Box
+      err := sof.Decode(buf)
       if err != nil {
          return err
       }
+      buf = buf[sof.BoxHeader.Size:]
+      
+      switch sof.BoxHeader.Type.String() {
+      case "stbl":
+         b.Stbl.BoxHeader = sof.BoxHeader
+         err := b.Stbl.Decode(sof.Payload)
+         if err != nil {
+            return err
+         }
+      case "dinf", // Roku
+         "smhd", // Roku
+         "vmhd": // Roku
+         b.Box = append(b.Box, sof)
+      default:
+         return &sofia.Error{b.BoxHeader, sof.BoxHeader}
+      }
    }
-   return b.Stbl.Write(dst)
+   return nil
 }

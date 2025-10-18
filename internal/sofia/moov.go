e52171611bfc7796f16576af7713fa6ab9e2f13f
27 <27@riseup.net>
2025-10-18 17:13:51 -0500

    modified:   internal/sofia/moov.go

--- internal/sofia/moov.go
+++ internal/sofia/moov.go
@@ -2,89 +2,89 @@ package mp4parser
 
 // MoovChildBox can hold any of the parsed child types or a raw box.
 type MoovChildBox struct {
-	Trak *TrakBox
-	Pssh *PsshBox
-	Raw  *RawBox
+   Trak *TrakBox
+   Pssh *PsshBox
+   Raw  *RawBox
 }
 
 // Size calculates the size of the contained child.
 func (c *MoovChildBox) Size() uint64 {
-	switch {
-	case c.Trak != nil:
-		return c.Trak.Size()
-	case c.Pssh != nil:
-		return c.Pssh.Size()
-	case c.Raw != nil:
-		return c.Raw.Size()
-	}
-	return 0
+   switch {
+   case c.Trak != nil:
+      return c.Trak.Size()
+   case c.Pssh != nil:
+      return c.Pssh.Size()
+   case c.Raw != nil:
+      return c.Raw.Size()
+   }
+   return 0
 }
 
 // Format formats the contained child.
 func (c *MoovChildBox) Format(dst []byte, offset int) int {
-	switch {
-	case c.Trak != nil:
-		return c.Trak.Format(dst, offset)
-	case c.Pssh != nil:
-		return c.Pssh.Format(dst, offset)
-	case c.Raw != nil:
-		return c.Raw.Format(dst, offset)
-	}
-	return offset
+   switch {
+   case c.Trak != nil:
+      return c.Trak.Format(dst, offset)
+   case c.Pssh != nil:
+      return c.Pssh.Format(dst, offset)
+   case c.Raw != nil:
+      return c.Raw.Format(dst, offset)
+   }
+   return offset
 }
 
 // MoovBox (Movie Box)
 type MoovBox struct {
-	Children []*MoovChildBox
+   Children []*MoovChildBox
 }
 
 // ParseMoovBox parses the MoovBox from its content slice.
 func ParseMoovBox(data []byte) (*MoovBox, error) {
-	b := &MoovBox{}
-	offset := 0
-	for offset < len(data) {
-		header, headerEndOffset, err := ParseBoxHeader(data, offset)
-		if err != nil {
-			return nil, err
-		}
-		contentEndOffset := offset + int(header.Size)
-		if contentEndOffset > len(data) {
-			return nil, ErrUnexpectedEOF
-		}
-		content := data[headerEndOffset:contentEndOffset]
-		child := &MoovChildBox{}
-		switch header.Type {
-		case "trak":
-			child.Trak, err = ParseTrakBox(content)
-		case "pssh":
-			child.Pssh, err = ParsePsshBox(content)
-		default:
-			child.Raw, err = ParseRawBox(header.Type, content)
-		}
-		if err != nil {
-			return nil, err
-		}
-		b.Children = append(b.Children, child)
-		offset = contentEndOffset
-	}
-	return b, nil
+   b := &MoovBox{}
+   offset := 0
+   for offset < len(data) {
+      header, headerEndOffset, err := ParseBoxHeader(data, offset)
+      if err != nil {
+         return nil, err
+      }
+      contentEndOffset := offset + int(header.Size)
+      if contentEndOffset > len(data) {
+         return nil, ErrUnexpectedEOF
+      }
+      content := data[headerEndOffset:contentEndOffset]
+      child := &MoovChildBox{}
+      switch header.Type {
+      case "trak":
+         child.Trak, err = ParseTrakBox(content)
+      case "pssh":
+         child.Pssh, err = ParsePsshBox(content)
+      default:
+         child.Raw, err = ParseRawBox(header.Type, content)
+      }
+      if err != nil {
+         return nil, err
+      }
+      b.Children = append(b.Children, child)
+      offset = contentEndOffset
+   }
+   return b, nil
 }
 
 // Size calculates the total byte size of the MoovBox.
 func (b *MoovBox) Size() uint64 {
-	size := uint64(8)
-	for _, child := range b.Children {
-		size += child.Size()
-	}
-	return size
+   size := uint64(8)
+   for _, child := range b.Children {
+      size += child.Size()
+   }
+   return size
 }
 
 // Format serializes the MoovBox into the destination slice.
 func (b *MoovBox) Format(dst []byte, offset int) int {
-	offset = writeUint32(dst, offset, uint32(b.Size()))
-	offset = writeString(dst, offset, "moov")
-	for _, child := range b.Children {
-		offset = child.Format(dst, offset)
-	}
-	return offset
+   offset = writeUint32(dst, offset, uint32(b.Size()))
+   offset = writeString(dst, offset, "moov")
+   for _, child := range b.Children {
+      offset = child.Format(dst, offset)
+   }
+   return offset
 }
--- internal/sofia/pssh.go
+++ internal/sofia/pssh.go
@@ -8,97 +8,97 @@ var WidevineSystemID = []byte{0xed, 0xef, 0x8b, 0xa9, 0x79, 0xd6, 0x4a, 0xce, 0x
 
 // PsshBox (Protection System Specific Header Box)
 type PsshBox struct {
-	FullBox
-	SystemID []byte // 16 bytes
-	KIDs     [][]byte // Optional, for Widevine v1+
-	Data     []byte
+   FullBox
+   SystemID []byte   // 16 bytes
+   KIDs     [][]byte // Optional, for Widevine v1+
+   Data     []byte
 }
 
 // ParsePsshBox parses the PsshBox from its content slice.
 func ParsePsshBox(data []byte) (*PsshBox, error) {
-	b := &PsshBox{}
-	offset, err := b.FullBox.Parse(data, 0)
-	if err != nil {
-		return nil, err
-	}
+   b := &PsshBox{}
+   offset, err := b.FullBox.Parse(data, 0)
+   if err != nil {
+      return nil, err
+   }
 
-	if offset+16 > len(data) {
-		return nil, ErrUnexpectedEOF
-	}
-	b.SystemID = data[offset : offset+16]
-	offset += 16
+   if offset+16 > len(data) {
+      return nil, ErrUnexpectedEOF
+   }
+   b.SystemID = data[offset : offset+16]
+   offset += 16
 
-	isWidevine := bytes.Equal(b.SystemID, WidevineSystemID)
+   isWidevine := bytes.Equal(b.SystemID, WidevineSystemID)
 
-	// Widevine pssh boxes have a more complex structure
-	if isWidevine {
-		if b.Version > 0 {
-			var kidCount uint32
-			kidCount, offset, err = readUint32(data, offset)
-			if err != nil {
-				return nil, err
-			}
-			for i := 0; i < int(kidCount); i++ {
-				if offset+16 > len(data) {
-					return nil, ErrUnexpectedEOF
-				}
-				b.KIDs = append(b.KIDs, data[offset:offset+16])
-				offset += 16
-			}
-		}
-		var dataSize uint32
-		dataSize, offset, err = readUint32(data, offset)
-		if err != nil {
-			return nil, err
-		}
-		if offset+int(dataSize) > len(data) {
-			return nil, ErrUnexpectedEOF
-		}
-		b.Data = data[offset : offset+int(dataSize)]
-	} else {
-		// For other systems like PlayReady, the rest of the box is data.
-		b.Data = data[offset:]
-	}
-	return b, nil
+   // Widevine pssh boxes have a more complex structure
+   if isWidevine {
+      if b.Version > 0 {
+         var kidCount uint32
+         kidCount, offset, err = readUint32(data, offset)
+         if err != nil {
+            return nil, err
+         }
+         for i := 0; i < int(kidCount); i++ {
+            if offset+16 > len(data) {
+               return nil, ErrUnexpectedEOF
+            }
+            b.KIDs = append(b.KIDs, data[offset:offset+16])
+            offset += 16
+         }
+      }
+      var dataSize uint32
+      dataSize, offset, err = readUint32(data, offset)
+      if err != nil {
+         return nil, err
+      }
+      if offset+int(dataSize) > len(data) {
+         return nil, ErrUnexpectedEOF
+      }
+      b.Data = data[offset : offset+int(dataSize)]
+   } else {
+      // For other systems like PlayReady, the rest of the box is data.
+      b.Data = data[offset:]
+   }
+   return b, nil
 }
 
 // Size calculates the total byte size of the PsshBox.
 func (b *PsshBox) Size() uint64 {
-	size := uint64(8) // Header
-	size += b.FullBox.Size()
-	size += 16 // SystemID
+   size := uint64(8) // Header
+   size += b.FullBox.Size()
+   size += 16 // SystemID
 
-	isWidevine := bytes.Equal(b.SystemID, WidevineSystemID)
-	if isWidevine {
-		if b.Version > 0 {
-			size += 4 // KIDCount
-			size += uint64(len(b.KIDs) * 16)
-		}
-		size += 4 // DataSize
-	}
+   isWidevine := bytes.Equal(b.SystemID, WidevineSystemID)
+   if isWidevine {
+      if b.Version > 0 {
+         size += 4 // KIDCount
+         size += uint64(len(b.KIDs) * 16)
+      }
+      size += 4 // DataSize
+   }
 
-	size += uint64(len(b.Data))
-	return size
+   size += uint64(len(b.Data))
+   return size
 }
 
 // Format serializes the PsshBox into the destination slice.
 func (b *PsshBox) Format(dst []byte, offset int) int {
-	offset = writeUint32(dst, offset, uint32(b.Size()))
-	offset = writeString(dst, offset, "pssh")
-	offset = b.FullBox.Format(dst, offset)
-	offset = writeBytes(dst, offset, b.SystemID)
+   offset = writeUint32(dst, offset, uint32(b.Size()))
+   offset = writeString(dst, offset, "pssh")
+   offset = b.FullBox.Format(dst, offset)
+   offset = writeBytes(dst, offset, b.SystemID)
 
-	isWidevine := bytes.Equal(b.SystemID, WidevineSystemID)
-	if isWidevine {
-		if b.Version > 0 {
-			offset = writeUint32(dst, offset, uint32(len(b.KIDs)))
-			for _, kid := range b.KIDs {
-				offset = writeBytes(dst, offset, kid)
-			}
-		}
-		offset = writeUint32(dst, offset, uint32(len(b.Data)))
-	}
+   isWidevine := bytes.Equal(b.SystemID, WidevineSystemID)
+   if isWidevine {
+      if b.Version > 0 {
+         offset = writeUint32(dst, offset, uint32(len(b.KIDs)))
+         for _, kid := range b.KIDs {
+            offset = writeBytes(dst, offset, kid)
+         }
+      }
+      offset = writeUint32(dst, offset, uint32(len(b.Data)))
+   }
 
-	offset = writeBytes(dst, offset, b.Data)
-	return offset
+   offset = writeBytes(dst, offset, b.Data)
+   return offset
 }

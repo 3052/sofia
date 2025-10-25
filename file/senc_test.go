91a5a2113b2bf07fb1a338491ab1ee4850ee69f0
27 <27@riseup.net>
2025-10-25 15:33:45 -0500

    new file:   testdata/hboMax-dvh1/28883-59832.mp4

--- file/senc_test.go
+++ file/senc_test.go
@@ -7,62 +7,67 @@ import (
    "testing"
 )
 
-const folder = "../testdata/"
+type senc_test struct {
+   initial string
+   key     string
+   out     string
+   segment string
+}
 
 var senc_tests = []senc_test{
    {
-      "criterion-avc1/0-804.mp4",
-      "criterion-avc1/13845-168166.mp4",
-      "377772323b0f45efb2c53c603749d834",
-      "criterion-avc1.mp4",
+      initial: "criterion-avc1/0-804.mp4",
+      key:     "377772323b0f45efb2c53c603749d834",
+      out:     "criterion-avc1.mp4",
+      segment: "criterion-avc1/13845-168166.mp4",
    },
    {
-      "hboMax-dvh1/0-902.mp4",
-      "hboMax-dvh1/903-28882.mp4",
-      "ee0d569c019057569eaf28b988c206f6",
-      "hboMax-dvh1.mp4",
+      initial: "hboMax-dvh1/0-902.mp4",
+      key:     "ee0d569c019057569eaf28b988c206f6",
+      out:     "hboMax-dvh1.mp4",
+      segment: "hboMax-dvh1/28883-59832.mp4",
    },
    {
-      "hboMax-ec-3/0-657.mp4",
-      "hboMax-ec-3/658-28709.mp4",
-      "acaec99945a3615c9ef7b1b04727022a",
-      "hboMax-ec-3.mp4",
+      initial: "hboMax-ec-3/0-657.mp4",
+      key:     "acaec99945a3615c9ef7b1b04727022a",
+      out:     "hboMax-ec-3.mp4",
+      segment: "hboMax-ec-3/658-28709.mp4",
    },
    {
-      "hboMax-hvc1/0-793.mp4",
-      "hboMax-hvc1/794-28773.mp4",
-      "bd691b57ac7c0620482c724b953a8e87",
-      "hboMax-hvc1.mp4",
+      initial: "hboMax-hvc1/0-793.mp4",
+      key:     "bd691b57ac7c0620482c724b953a8e87",
+      out:     "hboMax-hvc1.mp4",
+      segment: "hboMax-hvc1/794-28773.mp4",
    },
    {
-      "hulu-avc1/map.mp4",
-      "hulu-avc1/pts_0.mp4",
-      "33a7ef13ee16fa6a3d1467c0cc59a84f",
-      "hulu-avc1.mp4",
+      initial: "hulu-avc1/map.mp4",
+      key:     "33a7ef13ee16fa6a3d1467c0cc59a84f",
+      out:     "hulu-avc1.mp4",
+      segment: "hulu-avc1/pts_0.mp4",
    },
    {
-      "paramount-mp4a/init.m4v",
-      "paramount-mp4a/seg_1.m4s",
-      "d98277ff6d7406ec398b49bbd52937d4",
-      "paramount-mp4a.mp4",
+      initial: "paramount-mp4a/init.m4v",
+      key:     "d98277ff6d7406ec398b49bbd52937d4",
+      out:     "paramount-mp4a.mp4",
+      segment: "paramount-mp4a/seg_1.m4s",
    },
    {
-      "roku-avc1/index_video_8_0_init.mp4",
-      "roku-avc1/index_video_8_0_1.mp4",
-      "1ba08384626f9523e37b9db17f44da2b",
-      "roku-avc1.mp4",
+      initial: "roku-avc1/index_video_8_0_init.mp4",
+      key:     "1ba08384626f9523e37b9db17f44da2b",
+      out:     "roku-avc1.mp4",
+      segment: "roku-avc1/index_video_8_0_1.mp4",
    },
    {
-      "rtbf-avc1/vod-idx-3-video=300000.dash",
-      "rtbf-avc1/vod-idx-3-video=300000-0.dash",
-      "553b091b257584d3938c35dd202531f8",
-      "rtbf-avc1.mp4",
+      initial: "rtbf-avc1/vod-idx-3-video=300000.dash",
+      key:     "553b091b257584d3938c35dd202531f8",
+      out:     "rtbf-avc1.mp4",
+      segment: "rtbf-avc1/vod-idx-3-video=300000-0.dash",
    },
    {
-      "tubi-avc1/0-1683.mp4",
-      "tubi-avc1/1684-16523.mp4",
-      "8109222ffe94120d61f887d40d0257ed",
-      "tubi-avc1.mp4",
+      initial: "tubi-avc1/0-1683.mp4",
+      key:     "8109222ffe94120d61f887d40d0257ed",
+      out:     "tubi-avc1.mp4",
+      segment: "tubi-avc1/1684-16523.mp4",
    },
 }
 
@@ -71,15 +76,15 @@ func (s *senc_test) encode_init() ([]byte, error) {
    if err != nil {
       return nil, err
    }
-   var file1 File
-   err = file1.Read(data)
+   var fileVar File
+   err = fileVar.Read(data)
    if err != nil {
       return nil, err
    }
-   for _, pssh := range file1.Moov.Pssh {
+   for _, pssh := range fileVar.Moov.Pssh {
       copy(pssh.BoxHeader.Type[:], "free") // Firefox
    }
-   description := file1.Moov.Trak.Mdia.Minf.Stbl.Stsd
+   description := fileVar.Moov.Trak.Mdia.Minf.Stbl.Stsd
    if sinf, ok := description.Sinf(); ok {
       // Firefox
       copy(sinf.BoxHeader.Type[:], "free")
@@ -88,34 +93,7 @@ func (s *senc_test) encode_init() ([]byte, error) {
          copy(sample.BoxHeader.Type[:], sinf.Frma.DataFormat[:])
       }
    }
-   return file1.Append(nil)
-}
-
-func (s *senc_test) encode_segment(data []byte) ([]byte, error) {
-   log.Print(folder + s.segment)
-   segment, err := os.ReadFile(folder + s.segment)
-   if err != nil {
-      return nil, err
-   }
-   var file1 File
-   err = file1.Read(segment)
-   if err != nil {
-      return nil, err
-   }
-   track := file1.Moof.Traf
-   if senc := track.Senc; senc != nil {
-      key, err := hex.DecodeString(s.key)
-      if err != nil {
-         return nil, err
-      }
-      for i, data := range file1.Mdat.Data(&track) {
-         err := senc.Sample[i].Decrypt(data, key)
-         if err != nil {
-            return nil, err
-         }
-      }
-   }
-   return file1.Append(data)
+   return fileVar.Append(nil)
 }
 
 func TestSenc(t *testing.T) {
@@ -135,9 +113,31 @@ func TestSenc(t *testing.T) {
    }
 }
 
-type senc_test struct {
-   initial string
-   segment string
-   key     string
-   out     string
+func (s *senc_test) encode_segment(data []byte) ([]byte, error) {
+   log.Print(folder + s.segment)
+   segment, err := os.ReadFile(folder + s.segment)
+   if err != nil {
+      return nil, err
+   }
+   var fileVar File
+   err = fileVar.Read(segment)
+   if err != nil {
+      return nil, err
+   }
+   track := fileVar.Moof.Traf
+   if senc := track.Senc; senc != nil {
+      key, err := hex.DecodeString(s.key)
+      if err != nil {
+         return nil, err
+      }
+      for i, data := range fileVar.Mdat.Data(&track) {
+         err := senc.Sample[i].Decrypt(data, key)
+         if err != nil {
+            return nil, err
+         }
+      }
+   }
+   return fileVar.Append(data)
 }
+
+const folder = "../testdata/"

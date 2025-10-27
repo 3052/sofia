6ce8124f5150623eac400ee8c57459d5748667a6
27 <27@riseup.net>
2025-10-27 07:09:53 -0500

    new file:   internal/sofia/pssh_test.go

--- internal/sofia/mp4.go
+++ internal/sofia/mp4.go
@@ -1,7 +1,31 @@
 package mp4
 
-import "fmt"
+import (
+   "encoding/binary"
+   "errors"
+   "fmt"
+)
 
+type BoxHeader struct {
+   Size uint32
+   Type [4]byte
+}
+
+func ReadBoxHeader(data []byte) (BoxHeader, int, error) {
+   if len(data) < 8 {
+      return BoxHeader{}, 0, errors.New("not enough data for box header")
+   }
+   var h BoxHeader
+   h.Size = binary.BigEndian.Uint32(data[0:4])
+   copy(h.Type[:], data[4:8])
+   return h, 8, nil
+}
+
+func (h BoxHeader) Write(data []byte) int {
+   binary.BigEndian.PutUint32(data[0:4], h.Size)
+   copy(data[4:8], h.Type[:])
+   return 8
+}
 // Box is a generic wrapper for any top-level MP4 box.
 type Box struct {
    Moov *MoovBox

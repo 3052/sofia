db61d1cd4b9d78143918d0a8343f71d50d42d8e5
27 <27@riseup.net>
2026-03-18 17:07:09 -0500

    +type FrmaBox struct {

--- config.go
+++ config.go
@@ -95,6 +95,23 @@ func (b *EncBox) Unprotect() error {
    return nil
 }
 
+// --- FRMA ---
+type FrmaBox struct {
+   Header     BoxHeader
+   DataFormat [4]byte
+}
+
+func (b *FrmaBox) Parse(data []byte) error {
+   if err := b.Header.Parse(data); err != nil {
+      return err
+   }
+   if len(data) < 12 {
+      return errors.New("frma box is too small")
+   }
+   copy(b.DataFormat[:], data[8:12])
+   return nil
+}
+
 // --- SCHI (Scheme Information) ---
 type SchiBox struct {
    Header      BoxHeader
@@ -265,24 +282,5 @@ func (b *StsdBox) UnprotectAll() error {
          return err
       }
    }
-   return nil
-}
-
-///
-
-// --- FRMA ---
-type FrmaBox struct {
-   Header     BoxHeader
-   DataFormat [4]byte
-}
-
-func (b *FrmaBox) Parse(data []byte) error {
-   if err := b.Header.Parse(data); err != nil {
-      return err
-   }
-   if len(data) < 12 {
-      return errors.New("frma box is too small")
-   }
-   copy(b.DataFormat[:], data[8:12])
    return nil
 }
